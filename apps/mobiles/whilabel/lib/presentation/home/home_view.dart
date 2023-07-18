65f6b7f137f20b4ce5a0b3ebecb53f72f712dc19
email <joungminseong@Joungui-MacBookAir.local>
2023-07-18 11:54:56 +0900

    [FEATURE] add home_view.dart
    - 앱을 실행하면 가장 처음에 나올 페이지입니다.
    - 모든 페이지들의 시작점이 입니다.

---
+++ apps/mobiles/whilabel/lib/presentation/home/home_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:whilabel/presentation/resources/routes_manager.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("home 입니다"),
+        leading: const SizedBox(width: 0),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            TextButton(
+              onPressed: () {
+                Navigator.pushNamed(context, Routes.cameraRoute);
+              },
+              child: const Text("(test) go cameraView"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
