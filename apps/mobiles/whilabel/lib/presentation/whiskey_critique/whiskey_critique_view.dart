8a6cce0a39d4c0003e7e1e15cd438783e2a70737
hihibara <joungminseong@Joungui-MacBookAir.local>
2023-08-03 20:35:42 +0900

    [refactory] add comment on whiskey_critique_view.dart
    whiskey_critique.md에 내용을 주석으로 whiskey_critique_view.dart 상단
    에 추가하였습니다.

--- apps/mobiles/whilabel/lib/presentation/whiskey_critique/whiskey_critique_view.dart
+++ apps/mobiles/whilabel/lib/presentation/whiskey_critique/whiskey_critique_view.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+// 사용자가 사진을 찍은 위스키에 대해서 자신의 비펴을 적을 수 있는 view 입니다.
+// 필수 정보(위스키 별점, 한줄평)가 입력되어야 '저장하기" 버튼을 눌러서 다음 flow이동 가능
+
+// <안에 들어갈 내용>
+// - 위스키 별점 평가 (tab해서 5개 별들의 색상을 채우는 방식, 별 반개만 채우기)
+// - 위스키 한줄평 (Text입력해서 채우기)
+// - 테이스팅 노트 (Text입력해서 채우기)
+
 // 피그마에서 인식성공의 나의평가 부분 (자세한것은 이슈와 PR참고)
 class WhiskeyCritiqueView extends StatelessWidget {
   const WhiskeyCritiqueView({super.key});
