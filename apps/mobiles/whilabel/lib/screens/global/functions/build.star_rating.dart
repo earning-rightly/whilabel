4ee4a7dbe980e1209e85c0434d2098f796f189e6
hihibara <joungminseong@Joungui-MacBookAir.local>
2023-08-09 00:46:53 +0900

    [refactory] change to disable on buildStarRating parameter name
    파미터 이름에 변경으로 인해서 사용하는 방법도 달라져야 합니다.
    
    변동 사항
    - enable => disable
    - ignoreGestures: !enable, =>  ignoreGestures: disable,

--- apps/mobiles/whilabel/lib/screens/global/functions/build.star_rating.dart
+++ apps/mobiles/whilabel/lib/screens/global/functions/build.star_rating.dart
@@ -3,9 +3,9 @@ import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 // 5개의 별점 widget을 만들어주는 함수입니다. 0.5개도 가능합니다.
 // flutter flutter_rating_bar package입니다.
-Widget buildStarRating(double initialRating, bool enable) {
+Widget buildStarRating(double initialRating, bool disable) {
   return RatingBar.builder(
-    ignoreGestures: !enable, // 별점 매길 것 인지 보기만 할 것인지
+    ignoreGestures: disable, // 별점 매길 것 인지 보기만 할 것인지
     initialRating: initialRating, // 처음에 보여 줘야 할 별점 개수
     minRating: 0,
     direction: Axis.horizontal,
