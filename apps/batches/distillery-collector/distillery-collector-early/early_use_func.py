b2f1f57070be25df431c906c50d80e8dcbc64f8b
choejong-won <bmsyg987@gmail.com>
2023-08-09 01:35:34 +0900

    add func(scrap_distillery_collector_early) *add annotation

--- apps/batches/distillery-collector/distillery-collector-early/early_use_func.py
+++ apps/batches/distillery-collector/distillery-collector-early/early_use_func.py
@@ -4,15 +4,17 @@ import distillery_values
 import pandas as pd
 import json
 
-def scrap_distillery_collector_early():
+def scrap_distillery_collector_early():            #증류소의 초기 정보를 수집하는 함수
         scraper = cfscrape.create_scraper()
 
         soup = BeautifulSoup(scraper.get(distillery_values.distillery_collec_early_url_dict['distillery']).content, 'lxml')
-        clickable_list = soup.select(".clickable")
-        data_list = soup.select(".data")
+        #whisky base에 distillery 카테고리 홈페이지 정보 수집
+        clickable_list = soup.select(".clickable") #증류소 갯수 확인 type : list
 
-        for i in range(len(clickable_list)):
-            distillery_values.distillery_name_list.append(clickable_list[i].text)
+        data_list = soup.select(".data")           #data로 시작하는 class 명 수집 type : list
+
+        for i in range(len(clickable_list)):       #증류쇼 갯수만큼 반복
+            distillery_values.distillery_name_list.append(clickable_list[i].text) #증류소 초기정보 수집
             distillery_values.link_list.append(clickable_list[i].a['href'])
             distillery_values.country_list.append(data_list[4 * (i + 1)].text)
             distillery_values.whiskies_list.append(data_list[4 * (i + 1) + 1].text)
