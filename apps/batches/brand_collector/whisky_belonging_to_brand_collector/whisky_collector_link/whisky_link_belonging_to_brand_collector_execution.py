7f4936c2326891252216f5e15557ad09c0ced4bc
kor-jongwon <bmsyg987@gmail.com>
2023-08-10 21:32:15 +0900

    Update whisky_link_belonging_to_brand_collector_execution.py

--- apps/batches/brand_collector/whisky_belonging_to_brand_collector/whisky_collector_link/whisky_link_belonging_to_brand_collector_execution.py
+++ apps/batches/brand_collector/whisky_belonging_to_brand_collector/whisky_collector_link/whisky_link_belonging_to_brand_collector_execution.py
@@ -14,6 +14,6 @@ def whisky_link_brand_collector_executions(mode = 'brand', level = 'link'):
     print("start whisky_link_belonging_to_brand_collector")
     libs_func.write_log(current_time=libs_func.extract_time(), log_mode='start', mode=mode, level=level)  #시작 로그 기록
     whisky_collector_link_func.initserilized_value(mode)                                                  #위스키 링크 수집 함수 호출
-    libs_func.save_results(result_dict = whisky_collector_link_values.whisky_collec_link_scrap_dict, file_form = 'brand_whisky_collector_link')  #위스키 링크 수집 함수 호출
-    libs_func.write_log(current_time=libs_func.extract_time(), log_mode='end', mode = mode, level = level) #위스키 링크 수집 함수 호출
-    print("end whisky_link_belonging_to_brand_collector")                                                  #종료 로그 기록
+    libs_func.save_results(result_dict = whisky_collector_link_values.whisky_collec_link_scrap_dict, file_form = 'brand_whisky_collector_link')  #위스키 링크 저장
+    libs_func.write_log(current_time=libs_func.extract_time(), log_mode='end', mode = mode, level = level) #종료 로그 기록
+    print("end whisky_link_belonging_to_brand_collector")                                                  
