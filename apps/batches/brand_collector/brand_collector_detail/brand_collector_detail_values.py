124141e0ccaa0714a330d46f62c915d8a7b33e86
choejong-won <bmsyg987@gmail.com>
2023-08-10 19:04:25 +0900

    브랜드 상세 정보 수집에 사용되는 변수 선언

---
+++ apps/batches/brand_collector/brand_collector_detail/brand_collector_detail_values.py
@@ -0,0 +1,15 @@
+Country_list = []      #수집할 나라 리스트
+Website_list = []      #수집할 홈페이지 리스트
+Region_list = []       #수집할 지역 리스트
+
+brand_collec_detail_scrap_dict = {    #수집할 리스트를 딕셔너리(json)형식으로 변환
+ 'Country' : Country_list,
+ 'Website' : Website_list,
+ 'Region' : Region_list
+}
+
+brand_collec_detail_result_dict = {  #저장할 리스트를 딕셔너리(json)형식으로 변환
+ 'Country' : Country_list,
+ 'Website' : Website_list,
+ 'Region' : Region_list
+}
