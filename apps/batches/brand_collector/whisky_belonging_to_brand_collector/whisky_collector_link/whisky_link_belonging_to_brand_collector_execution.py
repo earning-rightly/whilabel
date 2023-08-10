c71e4353ac1dc36a420b0a4bd5091ea1ae7ca70b
choejong-won <bmsyg987@gmail.com>
2023-08-10 18:47:25 +0900

    브랜드에 속한 위스키 링크 수집을 실행하는 함수

---
+++ apps/batches/brand_collector/whisky_belonging_to_brand_collector/whisky_collector_link/whisky_link_belonging_to_brand_collector_execution.py
@@ -0,0 +1,19 @@
+from batches.whisky_collector.whisky_collector_link import whisky_collector_link_func
+from batches.whisky_collector.whisky_collector_link import whisky_collector_link_values
+from batches.libs import libs_func
+
+def whisky_link_brand_collector_executions(mode = 'brand', level = 'link'):  #mode allows you to choose between distilleries and brands to collect { mode : distillery, brand} * default = distillery
+    """
+       whisky_link_brand_collector_executions.
+           Args:
+               mode : mode값을 받아, 하위 함수르 호출할때 현재 상태를 전달.
+               detail : detail값을 받아, 하위 함수르 호출할때 현재 상태를 전달.
+           Note:
+               scrap_main 에서 위스키 링크  스케줄링 함수
+    """
+    print("start whisky_link_belonging_to_brand_collector")
+    libs_func.write_log(current_time=libs_func.extract_time(), log_mode='start', mode=mode, level=level)  #시작 로그 기록
+    whisky_collector_link_func.initserilized_value(mode)                                                  #위스키 링크 수집 함수 호출
+    libs_func.save_results(result_dict = whisky_collector_link_values.whisky_collec_link_scrap_dict, file_form = 'brand_whisky_collector_link')  #위스키 링크 수집 함수 호출
+    libs_func.write_log(current_time=libs_func.extract_time(), log_mode='end', mode = mode, level = level) #위스키 링크 수집 함수 호출
+    print("end whisky_link_belonging_to_brand_collector")                                                  #종료 로그 기록
