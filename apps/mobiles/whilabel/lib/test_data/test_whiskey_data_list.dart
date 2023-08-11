07ccf7c172a677ae49108e96caf097e687e24277
2821char <109503403+hihibara@users.noreply.github.com>
2023-08-11 16:38:58 +0900

    DB 구성전까지 사용될 MockData (#35)
    * [feature] Add TestWhiskeyDataModel
    
    아직 정의된 데이터모델이 없기에 화면구성에서 필요로 만들었다.
    
    * [feature] Add testWhiskeyDataList
    
    - TestWhiskeyDataModel을 type을 원소로 갇는 리스트.
    - DB가 만들어지기 전이라서 사용
    
    ---------
    
    Co-authored-by: hihibara <joungminseong@Joungui-MacBookAir.local>

---
+++ apps/mobiles/whilabel/lib/test_data/test_whiskey_data_list.dart
@@ -0,0 +1,58 @@
+import 'package:whilabel/test_data/test_whiskey_data_model.dart';
+
+final List<TestWhiskeyDataModel> testWhiskeyDataList = [
+  TestWhiskeyDataModel(
+    imageUrl:
+        "https://cdn11.bigcommerce.com/s-7a906/images/stencil/1280x1280/products/1325/6500/jack_daniels_old_7__90093.1516309412.jpg?c=2&imbypass=on",
+    whiskeyName: "test0",
+    starRating: 2,
+    whiskeyProducingArea: "test country",
+    whiskeyLcoholPercentagea: 40,
+    oneLineText: "테스트 중입니다, 테스트 중입니다",
+  ),
+  TestWhiskeyDataModel(
+    imageUrl:
+        "https://cdn11.bigcommerce.com/s-7a906/images/stencil/1280x1280/products/1325/6500/jack_daniels_old_7__90093.1516309412.jpg?c=2&imbypass=on",
+    whiskeyName: "test1",
+    starRating: 2,
+    whiskeyProducingArea: "test country",
+    whiskeyLcoholPercentagea: 40,
+    oneLineText: "테스트 중입니다, 테스트 중입니다",
+  ),
+  TestWhiskeyDataModel(
+    imageUrl:
+        "https://cdn11.bigcommerce.com/s-7a906/images/stencil/1280x1280/products/1325/6500/jack_daniels_old_7__90093.1516309412.jpg?c=2&imbypass=on",
+    whiskeyName: "test2",
+    starRating: 2,
+    whiskeyProducingArea: "test country",
+    whiskeyLcoholPercentagea: 40,
+    oneLineText: "테스트 중입니다, 테스트 중입니다",
+  ),
+  TestWhiskeyDataModel(
+    imageUrl:
+        "https://cdn11.bigcommerce.com/s-7a906/images/stencil/1280x1280/products/1325/6500/jack_daniels_old_7__90093.1516309412.jpg?c=2&imbypass=on",
+    whiskeyName: "test3",
+    starRating: 2,
+    whiskeyProducingArea: "test country",
+    whiskeyLcoholPercentagea: 40,
+    oneLineText: "테스트 중입니다, 테스트 중입니다",
+  ),
+  TestWhiskeyDataModel(
+    imageUrl:
+        "https://cdn11.bigcommerce.com/s-7a906/images/stencil/1280x1280/products/1325/6500/jack_daniels_old_7__90093.1516309412.jpg?c=2&imbypass=on",
+    whiskeyName: "test4",
+    starRating: 2,
+    whiskeyProducingArea: "test country",
+    whiskeyLcoholPercentagea: 40,
+    oneLineText: "테스트 중입니다, 테스트 중입니다",
+  ),
+  TestWhiskeyDataModel(
+    imageUrl:
+        "https://cdn11.bigcommerce.com/s-7a906/images/stencil/1280x1280/products/1325/6500/jack_daniels_old_7__90093.1516309412.jpg?c=2&imbypass=on",
+    whiskeyName: "test5",
+    starRating: 2,
+    whiskeyProducingArea: "test country",
+    whiskeyLcoholPercentagea: 40,
+    oneLineText: "테스트 중입니다, 테스트 중입니다",
+  ),
+];
