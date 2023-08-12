8c1d91878783babb7ad5c8320e62e3d1132f28bd
choejong-won <bmsyg987@gmail.com>
2023-08-12 15:50:26 +0900

    <신규추가> description : whisk base사이트내 브랜드 링크(whisky base에 기재된 브랜드 정보)에서 사전 정보(*_pre)에서는 확인하지 못한 브랜드에 보다 더 자세한 정보   (브랜드의 지역, 브랜드(자체) 홈페이지) 을 추출합니다.
    1. wb_brand_collector_detail_execution.py
       -> *_detail_func.py를 실행하기위한 함수로써 메인 스케줄링 함수에서 직접적으로 호출되는 함수 입니다.

---
+++ apps/batches/wb_brand_collector/wb_brand_collector_detail/wb_brand_collector_detail_execution.py
@@ -0,0 +1,20 @@
+from batches.wb_brand_collector.wb_brand_collector_detail import wb_brand_collector_detail_func
+from batches.wb_brand_collector.wb_brand_collector_detail.wb_brand_collector_detail_values import brand_collect_detail_scrap as detail_scrap
+from batches.wb_libs import wb_libs_func
+
+
+def brand_detail_executions(mode = 'brand', level = 'detail'):  #mode allows you to choose between distilleries and brands to collect { mode : distillery, brand} * default = distillery
+    """
+        brand_detail_executions.
+            Args:
+                mode : mode값을 받아, 하위 함수르 호출할때 현재 상태를 전달.
+                detail : detail값을 받아, 하위 함수르 호출할때 현재 상태를 전달.
+            Note:
+                scrap_main 에서 브랜드 상세정보 스케줄링 함수
+    """
+    print("start wb_brand_collector_detail")
+    wb_libs_func.write_log(current_time=wb_libs_func.extract_time(), log_mode='start', mode = mode, level = level) #시작 로그 기록
+    wb_brand_collector_detail_func.collect()                                                                       #브랜드 상세정보 수집 함수 호출
+    wb_libs_func.save_results(result_dict = detail_scrap, file_form ='wb_brand_collector_detail')                  #브랜드 상세정보 저장 함수 호출
+    wb_libs_func.write_log(current_time=wb_libs_func.extract_time(), log_mode='end', mode = mode, level = level)   #종료 로그 기록
+    print("end wb_brand_collector_detail")
