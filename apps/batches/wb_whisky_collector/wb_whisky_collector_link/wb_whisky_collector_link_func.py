e22daa629c25978e75cf21f43d45d0789817f9ba
choejong-won <bmsyg987@gmail.com>
2023-08-16 17:22:01 +0900

    <변경사항> aiohttp.client_exceptions.ClientConnectorError: 발생시 selenuim으로 연결 turn_around_selenum() 가 추가되었습니다

--- apps/batches/wb_whisky_collector/wb_whisky_collector_link/wb_whisky_collector_link_func.py
+++ apps/batches/wb_whisky_collector/wb_whisky_collector_link/wb_whisky_collector_link_func.py
@@ -1,5 +1,8 @@
 import asyncio
+import random
 import ssl
+import traceback
+import aiohttp.client_exceptions
 from bs4 import BeautifulSoup
 import pandas as pd
 from batches.wb_whisky_collector.wb_whisky_collector_link.wb_whisky_collector_link_values import whisky_collect_link_scrap as link_scrap
@@ -7,32 +10,61 @@ from tqdm.asyncio import tqdm_asyncio
 from aiohttp import ClientSession
 
 
-async def extract_whiksy_link_collector(url, sema, loop):
+async def extract_whiksy_link_collector(url_index,url, sema, loop):
     """
-            extract_whiksy_link_collector.
-                Args:
-                    url : 수집해야할 홈페이지 링크.
-                    sema : 비동기식 해당 스레드 위치주소.
-                    loop :  (async_execution)함수에서 asyncio.get_event_loop로 생성한 loop값
-                Note:
-                    증류소, 브랜드 에서 위스키 링크를 수집하기 위해 비동기식 처리를 위한 함수
-                Raises:
-                   aiohttp.client_exceptions.ClientConnectorError : ssl connect 문제 (해결 진행중)
-        """
+        extract_whiksy_link_collector.
+            Args:
+                url : 수집해야할 홈페이지 링크.
+                sema : 비동기식 해당 스레드 위치주소.
+                loop :  (async_execution)함수에서 asyncio.get_event_loop로 생성한 loop값
+            Note:
+                증류소, 브랜드 에서 위스키 링크를 수집하기 위해 비동기식 처리를 위한 함수
+            Raises:
+               aiohttp.client_exceptions.ClientConnectorError : ssl connect 문제 (해결 진행중)
+    """
+
+    def extract_whisky_link(soup):
+        photo_list = soup.select(".photo.buttons")
+        link = soup.select(".clickable")
+        for i in range(len(photo_list)):  # 위스키 사진기준으로 위스키 갯수를 파악후 반복문으로 링크 수집
+
+            # clickable이라는 클래명을 가진 태그에서 위스키 링크 추출하여  whikie_link 리스트에 저장
+            link_scrap['whisky_link'].append(link[i]['href'])
+
     async with sema:
         sslcontext = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
         async with ClientSession(trust_env=True) as session:
-            async with session.get(url, ssl=sslcontext)as response:
-                await asyncio.sleep(3)  # 세션 차단을 방지하기위한 3초 딜레이
-                r = await response.read()
-                soup = await loop.run_in_executor(None, BeautifulSoup, r, 'lxml')
-                photo_list = soup.select(".photo.buttons")
-                link = soup.select(".clickable")
-                for i in range(len(photo_list)):  # 위스키 사진기준으로 위스키 갯수를 파악후 반복문으로 링크 수집
+            await asyncio.sleep(random.randrange(5,60))  # 세션 차단을 방지하기위한  15초내외로 난수 딜레이
+            try:
 
-                 # clickable이라는 클래명을 가진 태그에서 위스키 링크 추출하여  whikie_link 리스트에 저장
-                    link_scrap['whisky_link'].append(link[i]['href'])
+                async with session.get(url, ssl=sslcontext)as response:
+                    r = await response.read()
+                    soup = await loop.run_in_executor(None, BeautifulSoup, r, 'lxml')
+                    extract_whisky_link(soup)
+            except aiohttp.client_exceptions.ClientConnectorError:  # 비동기식 접속으로 443포트에 대한 접속이 안되는 에러에 대한 예외처리
 
+                from selenium import webdriver
+                import chromedriver_autoinstaller
+                from selenium.webdriver.chrome.options import Options
+                print('used selenuim!!')
+                print(url_index)
+                chrome_options = Options()
+                # headless 설정
+
+                chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+                # and if it doesn't exist, download it automatically,
+                # then add chromedriver to path
+                chrome_options.add_argument("--headless=new")  # 크롬 창 실행없이 백그라운드에서 실행
+                driver = webdriver.Chrome(options=chrome_options)
+                driver.get(url[:url.rfind(('/'))] + '/about')
+                driver.implicitly_wait(3)
+                soup = await loop.run_in_executor(None, BeautifulSoup, driver.page_source,
+                                                  'lxml')  # BeautifulSoup을 통해 수집된 페이지 검색
+                extract_whisky_link(soup=soup)
+                driver.close()
+            except:
+                # write error_log.txt
+                traceback.print_exc()
 
 async def extract_whisky_link_collector_async(link, loop):
     """
@@ -43,8 +75,8 @@ async def extract_whisky_link_collector_async(link, loop):
             Note:
                 비동식 제한 갯수 설정(Semaphore) 및 수집해야할 홈페이지 반복문 실행
     """
-    semaphore = asyncio.Semaphore(10)
-    fts = [asyncio.ensure_future(extract_whiksy_link_collector(u, semaphore,loop)) for u in link]
+    semaphore = asyncio.Semaphore(30)
+    fts = [asyncio.ensure_future(extract_whiksy_link_collector(url_index,u, semaphore,loop)) for url_index, u in enumerate(link)]
     await tqdm_asyncio.gather(*fts)
 def collect(mode):
     """
