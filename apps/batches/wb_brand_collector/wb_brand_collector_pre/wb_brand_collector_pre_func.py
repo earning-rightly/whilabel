7e18e815d042dceffc2a6b485ddccebd8dc8ec09
choejong-won <bmsyg987@gmail.com>
2023-08-16 17:59:39 +0900

    values.py 변수명 변경으로 인한 코드 수정

--- apps/batches/wb_brand_collector/wb_brand_collector_pre/wb_brand_collector_pre_func.py
+++ apps/batches/wb_brand_collector/wb_brand_collector_pre/wb_brand_collector_pre_func.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import cfscrape
-from batches.wb_brand_collector.wb_brand_collector_pre.wb_brand_collector_pre_values import brand_collect_pre_url as url , brand_collect_pre_scrap as pre_scrap
+from batches.wb_brand_collector.wb_brand_collector_pre.wb_brand_collector_pre_values import brand_summary_collect_url as url , brand_summary_collect_data as pre_scrap
 
 def collect():    #브랜드의 초기 정보를 수집하는 함수
         """
@@ -9,8 +9,10 @@ def collect():    #브랜드의 초기 정보를 수집하는 함수
                     브랜드 사전 정보 수집 함수
         """
         scraper = cfscrape.create_scraper()
-        soup = BeautifulSoup(scraper.get(
-            url['brand']).content, 'lxml')
+        soup = BeautifulSoup(
+                scraper.get(url['brand']).content,
+                'lxml'
+                )
         #whisky base에 brands 카테고리 홈페이지 정보 수집
         clickable_list = soup.select(".clickable") #태그안 클래스명이 clickable인경우 추출, Returns : list
         data_list = soup.select(".data")           #태그안 클래스명이 data 추출, Returns : list
