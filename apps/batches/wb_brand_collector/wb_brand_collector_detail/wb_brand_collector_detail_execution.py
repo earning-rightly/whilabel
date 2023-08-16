2f45df4597ab854a2a347c5ca88d9bdbd02e47eb
kor-jongwon <bmsyg987@gmail.com>
2023-08-16 18:21:50 +0900

    Update wb_brand_collector_detail_execution.py

--- apps/batches/wb_brand_collector/wb_brand_collector_detail/wb_brand_collector_detail_execution.py
+++ apps/batches/wb_brand_collector/wb_brand_collector_detail/wb_brand_collector_detail_execution.py
@@ -1,6 +1,6 @@
-from batches.wb_brand_collector.wb_brand_collector_detail import wb_brand_collector_detail_func
-from batches.wb_brand_collector.wb_brand_collector_detail.wb_brand_collector_detail_values import brand_collect_detail_scrap as detail_scrap
-from batches.wb_libs import wb_libs_func
+from apps.batches.wb_brand_collector.wb_brand_collector_detail import wb_brand_collector_detail_func
+from apps.batches.wb_brand_collector.wb_brand_collector_detail.wb_brand_collector_detail_values import brand_collect_detail_scrap as detail_scrap
+from apps.batches.wb_libs import wb_libs_func
 
 
 def brand_detail_executions(mode = 'brand', level = 'detail'):  #mode allows you to choose between distilleries and brands to collect { mode : distillery, brand} * default = distillery
@@ -15,6 +15,6 @@ def brand_detail_executions(mode = 'brand', level = 'detail'):  #mode allows you
     print("start wb_brand_collector_detail")
     wb_libs_func.write_log(current_time=wb_libs_func.extract_time(), log_mode='start', mode = mode, level = level) #시작 로그 기록
     wb_brand_collector_detail_func.collect()                                                                       #브랜드 상세정보 수집 함수 호출
-    wb_libs_func.save_resconvertcsvtojson(result_dict = detail_scrap, file_form ='wb_brand_collector_detail')                  #브랜드 상세정보 저장 함수 호출
+    wb_libs_func.save_resconvert_csv_to_json(result_dict = detail_scrap, file_form ='wb_brand_collector_detail')                  #브랜드 상세정보 저장 함수 호출
     wb_libs_func.write_log(current_time=wb_libs_func.extract_time(), log_mode='end', mode = mode, level = level)   #종료 로그 기록
     print("end wb_brand_collector_detail")
