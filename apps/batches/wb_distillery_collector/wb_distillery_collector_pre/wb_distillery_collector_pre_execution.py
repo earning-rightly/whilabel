ac4e2b5588d8dd6c0ade4f4d4f2a5cc77f80018f
choejong-won <bmsyg987@gmail.com>
2023-08-16 17:38:14 +0900

    <변경사항> 1. wb_libs save_results -> save_resconvert_csv_to_json 함수명 변경으로 코드 수정

--- apps/batches/wb_distillery_collector/wb_distillery_collector_pre/wb_distillery_collector_pre_execution.py
+++ apps/batches/wb_distillery_collector/wb_distillery_collector_pre/wb_distillery_collector_pre_execution.py
@@ -1,6 +1,6 @@
-from apps.batches.wb_distillery_collector.wb_distillery_collector_pre import wb_distillery_collector_pre_func
-from apps.batches.wb_distillery_collector.wb_distillery_collector_pre.wb_distillery_collector_pre_values import distillery_collect_pre_scrap as pre_scrap
-from apps.batches.wb_libs import wb_libs_func
+from batches.wb_distillery_collector.wb_distillery_collector_pre import wb_distillery_collector_pre_func
+from batches.wb_distillery_collector.wb_distillery_collector_pre.wb_distillery_collector_pre_values import distillery_summary_collect_data as pre_scrap
+from batches.wb_libs import wb_libs_func
 def distillery_pre_executions(mode = 'distillery', level = 'early'):
     """
        distillery_pre_executions.
@@ -13,6 +13,6 @@ def distillery_pre_executions(mode = 'distillery', level = 'early'):
     print("start wb_distillery_collector_pre")
     wb_libs_func.write_log(current_time=wb_libs_func.extract_time(), log_mode='start', mode = mode, level = level)  #시작 로그 기록
     wb_distillery_collector_pre_func.collect()                                                                      #증류소 사전정보 수집 함수 호출
-    wb_libs_func.save_results(result_dict = pre_scrap, file_form ='wb_distillery_collector_pre')                    #증류소 사전정보 저장 함수 호출
+    wb_libs_func.save_resconvert_csv_to_json(result_dict = pre_scrap, file_form ='wb_distillery_collector_pre')                    #증류소 사전정보 저장 함수 호출
     wb_libs_func.write_log(current_time=wb_libs_func.extract_time(), log_mode='start', mode = mode, level = level)  #종료 로그 기록
     print("end wb_distillery_collector_pre")
