474b4b77c52e7f4b19b62a79380860b98ea922cf
choejong-won <bmsyg987@gmail.com>
2023-08-16 17:47:52 +0900

    *_executions.py 변경사항 save_results -> save_resconvert_csv_to_json으로 변경

--- apps/batches/wb_distillery_collector/wb_distillery_collector_detail/wb_distillery_collector_detail_execution.py
+++ apps/batches/wb_distillery_collector/wb_distillery_collector_detail/wb_distillery_collector_detail_execution.py
@@ -1,6 +1,6 @@
-from batches.wb_libs import wb_libs_func
-from batches.wb_distillery_collector.wb_distillery_collector_detail import wb_distillery_collector_detail_func
-from batches.wb_distillery_collector.wb_distillery_collector_detail.wb_distillery_collector_detail_values import distillery_collect_detail_scrap as detail_scrap
+from apps.batches.wb_libs import wb_libs_func
+from apps.batches.wb_distillery_collector.wb_distillery_collector_detail import wb_distillery_collector_detail_func
+from apps.batches.wb_distillery_collector.wb_distillery_collector_detail.wb_distillery_collector_detail_values import distillery_collect_detail_scrap as detail_scrap
 
 def distillery_detail_executions(mode = 'distillery', level = 'detail'):
     """
@@ -14,6 +14,6 @@ def distillery_detail_executions(mode = 'distillery', level = 'detail'):
     print("start wb_distillery_collector_detail")
     wb_libs_func.write_log(current_time=wb_libs_func.extract_time(), log_mode='start', mode = mode, level = level) #시작 로그 기록
     wb_distillery_collector_detail_func.collect()                                                                  #증류소 상세정보 수집 함수 호출
-    wb_libs_func.save_results(result_dict = detail_scrap, file_form ='wb_distillery_collector_detail')             #증류소 상세정보 저장 함수 호출
+    wb_libs_func.save_resconvert_csv_to_json(result_dict = detail_scrap, file_form ='wb_distillery_collector_detail')             #증류소 상세정보 저장 함수 호출
     wb_libs_func.write_log(current_time=wb_libs_func.extract_time(), log_mode='end', mode = mode, level = level)   #종료 로그 기록
     print("end wb_distillery_collector_detail")
