964f7a23ca507c4566c5d2f9a4121ef183b32057
hihibara <joungminseong@Joungui-MacBookAir.local>
2023-08-28 10:01:53 +0900

    [feature] Add LoginEvent()
    LoginViewModel를 호출해서 로직에 접근할수 있도록해주는 역활을합니다.
    screen와 domain이 독립적이게 만들어 줍니다

---
+++ apps/mobiles/whilabel/lib/screens/login/view_model/login_event.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:whilabel/data/user/sns_type.dart';
+
+part 'login_event.freezed.dart';
+
+@freezed
+abstract class LoginEvent with _$LoginEvent {
+  const factory LoginEvent.login(SnsType snsType) = Login;
+  const factory LoginEvent.logout() = Logout;
+}
