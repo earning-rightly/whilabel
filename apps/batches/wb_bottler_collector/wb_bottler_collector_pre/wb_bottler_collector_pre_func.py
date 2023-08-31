156c53879ff42e2c3f13f733849ed8be56d941b4
kor-jongwon <bmsyg987@gmail.com>
2023-08-31 09:20:22 +0900

    [feature] 3. 브랜드, 증류소, bottler 사전정보 수집 (#72)
    * 관련 이슈
    
    변경 사항 및 이유
    *_pre_execution.py -> *_pre_directed_acyclic_execution.py 파일명 변경
    pip 라이브러리와 package 구분하기위해 import 순서 조정
    
    PR Point
    참고 사항
    
    * bottler 주석 정보 수정

--- apps/batches/wb_bottler_collector/wb_bottler_collector_pre/wb_bottler_collector_pre_func.py
+++ apps/batches/wb_bottler_collector/wb_bottler_collector_pre/wb_bottler_collector_pre_func.py
@@ -2,9 +2,8 @@ from bs4 import BeautifulSoup
 import cfscrape
 from tqdm import tqdm
 
-from batches.wb_distillery_collector.wb_distillery_collector_pre.wb_distillery_collector_pre_values import \
-    distillery_summary_collect_url as url, distillery_summary_collect_data as pre_scrap
-
+from apps.batches.wb_bottler_collector.wb_bottler_collector_pre.wb_bottler_collector_pre_values import \
+    bottler_summary_collect_url as url, bottler_summary_collect_data as pre_scrap
 
 def collect():  # 증류소의 초기 정보를 수집하는 함수
     """
@@ -14,13 +13,13 @@ def collect():  # 증류소의 초기 정보를 수집하는 함수
     """
     scraper = cfscrape.create_scraper()
     soup = BeautifulSoup(
-        scraper.get(url=url['distillery']).content,
-        features='lxml')
+        scraper.get(url=url['bottler']).content,
+                    features='lxml')
     # whisky base에 distillery 카테고리 홈페이지 정보 수집
     clickable_list = soup.select(".clickable")  # 증류소 갯수 확인 type : list
     data_list = soup.select(".data")  # data로 시작하는 class 명 수집 type : list
     for i in tqdm(range(len(clickable_list))):  # 증류쇼 갯수만큼 반복
-        pre_scrap['distillery_name'].append(clickable_list[i].text.replace("\t", "").replace("\n", ""))  # 브랜드 초기정보 수집
+        pre_scrap['bottler_name'].append(clickable_list[i].text.replace("\t", "").replace("\n", ""))  # 브랜드 초기정보 수집
         pre_scrap['link'].append(clickable_list[i].a['href'])
         pre_scrap['country'].append(data_list[4 * (i + 1)].text)
         pre_scrap['whiskies'].append(data_list[(4 * (i + 1)) + 1].text)
