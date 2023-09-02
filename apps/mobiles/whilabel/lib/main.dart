1dfc97ea7c44adc82d94aa7749720518e8c84eed
hihibara <joungminseong@Joungui-MacBookAir.local>
2023-09-02 21:14:08 +0900

    [feature] Add whilable inital setting
    앱을 실행하기 전에 필요한 설정을 추가하였습니다

--- apps/mobiles/whilabel/lib/main.dart
+++ apps/mobiles/whilabel/lib/main.dart
@@ -1,20 +1,42 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+import 'package:provider/provider.dart';
 import 'package:whilabel/firebase_options.dart';
+import 'package:whilabel/initial_provider.dart';
 import 'package:whilabel/screens/constants/routes_manager.dart';
 import 'package:whilabel/screens/constants/themedata_manager.dart';
 
 void main() async {
+  await dotenv.load(fileName: 'assets/config/.env');
+
+  await Hive.initFlutter(); // * Hive 초기화
+  KakaoSdk.init(nativeAppKey: dotenv.get("KAKAO_NATIVE_APP_KEY"));
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MainApp());
+
+  runApp(
+    MultiProvider(
+      providers: initialProviders(),
+      child: MainApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MainApp extends StatefulWidget {
+  MainApp._internal();
+  static final MainApp instance = MainApp._internal();
+  factory MainApp() => instance; // to guarantee Sigleton
 
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
