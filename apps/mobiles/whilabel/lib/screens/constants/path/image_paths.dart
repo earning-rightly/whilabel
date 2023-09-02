a16b198ec0d68042ae00df3d1eb01961dea64655
2821char <109503403+hihibara@users.noreply.github.com>
2023-09-02 16:33:14 +0900

    [feature ]LoginView() 추가  (#69)
    * [feature] Add image used on LoginView
    
    loginview에서 사용하는 이미지 파일들 입니다.
    
     <추가된 이미지 파일>
        1. login_background.png
        2. Whilabel.svg
        3. google_login.svg
        4. instargram_login.svg
        5. kakao_login.svg
    
    * [feature] add image files path & packages
    
    < 추가한 기능들 >
     1. pubspec.yaml에 LoginView에서 사용할 이미지 파일 경로
     2. SVG 이미지를 사용할 수 있게해주는 패키지
    
    * [feature] Add path & style directories on constants
    
    path  (dirt): 앱 로컬에서 사용하는 image의 경로를 저장할 파일을 보관할 디렉터리
    style (dirt): UI  widget의 style 데이터 파일을 보관할 디렉터리
    
    * [feature] Add LoginView
    
      유저가 로그인을 하고자 할때 보여줄 페이지입니다.
    
      <내부 구성>
       1. 카카오 로그인
       2. 구글 로그인
       3. 인스타 로그인
    
    * [feature] Add EachLoginButton()
    
    LoginView()에서 사용되는 로그인 버튼 형식입니다.
    
    ---------
    
    Co-authored-by: hihibara <joungminseong@Joungui-MacBookAir.local>

---
+++ apps/mobiles/whilabel/lib/screens/constants/path/image_paths.dart
@@ -0,0 +1,5 @@
+const loginPngImage = "assets/login/login_background.png";
+const instargramIcon = "assets/login/instargram_login.svg";
+const kakaoIcon = "assets/login/kakao_login.svg";
+const googleIcon = "assets/login/google_login.svg";
+const whilableIcon = "assets/login/Whilabel.svg";
