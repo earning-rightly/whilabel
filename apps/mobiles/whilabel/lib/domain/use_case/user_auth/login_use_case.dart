b1507c8c4b9db0cef5796189a11f32063531a3cb
hihibara <joungminseong@Joungui-MacBookAir.local>
2023-09-02 19:01:40 +0900

    [fix] login & logout use case

--- apps/mobiles/whilabel/lib/domain/use_case/user_auth/login_use_case.dart
+++ apps/mobiles/whilabel/lib/domain/use_case/user_auth/login_use_case.dart
@@ -6,10 +6,10 @@ import 'package:whilabel/data/user/app_user.dart';
 import 'package:whilabel/data/user/auth_user.dart';
 import 'package:whilabel/data/user/sns_type.dart';
 import 'package:whilabel/domain/functions/create_firebase_token.dart';
+import 'package:whilabel/domain/global_provider/current_user_state.dart';
 import 'package:whilabel/domain/login_services/googel_oauth.dart';
 import 'package:whilabel/domain/login_services/instargram_oauth.dart';
 import 'package:whilabel/domain/login_services/kakao_oauth.dart';
-import 'package:whilabel/domain/share_provider/current_user_state.dart';
 import 'package:whilabel/domain/user/app_user_repository.dart';
 
 class LoginUseCase {
@@ -30,28 +30,32 @@ class LoginUseCase {
   });
 
   Future<Pair<bool, bool>> call(SnsType snsType) async {
-    // _isLogined = await loginRepository.login();
     switch (snsType) {
       case SnsType.KAKAO:
-        final loginUserInfo = await KaKaoOauth().login();
-        if (loginUserInfo != null) _loginUserInfo = loginUserInfo;
-        await _customTokenLoginService(_loginUserInfo);
+        final kakaoLoginedUserInfo = await KaKaoOauth().login();
+        if (kakaoLoginedUserInfo != null) _loginUserInfo = kakaoLoginedUserInfo;
 
         break;
+
       case SnsType.INSTAGRAM:
-        final loginUserInfo = await InstargramOauth().login();
-        if (loginUserInfo != null) _loginUserInfo = loginUserInfo;
-        await _customTokenLoginService(_loginUserInfo);
+        final instargramLoginedUserInfo = await InstargramOauth().login();
+        if (instargramLoginedUserInfo != null)
+          _loginUserInfo = instargramLoginedUserInfo;
 
         break;
+
       case SnsType.GOOGLE:
-        final loginUserInfo = await GoogleOauth().login();
-        if (loginUserInfo != null) _loginUserInfo = loginUserInfo;
+        final googleLoginedUserInfo = await GoogleOauth().login();
+        if (googleLoginedUserInfo != null)
+          _loginUserInfo = googleLoginedUserInfo;
+
+        break;
 
       default:
         debugPrint("snsType 값을 찾을 수 없습니다.");
         return Pair(false, false);
     }
+    await _customTokenLoginService(_loginUserInfo);
 
     currentUserStatus.updateUserState();
     bool isNewbie = await _isNewbie();
@@ -62,10 +66,9 @@ class LoginUseCase {
   Future<bool> _isNewbie() async {
     final currentUser = await appUserRepository.getCurrentUser();
 
-    if (currentUser != null &&
-        (currentUser.uid.isNotEmpty || currentUser.name.isNotNullOrEmpty)) {
+    if (currentUser != null && (currentUser.name.isNotNullOrEmpty)) {
       debugPrint("current user is not new  ");
-      return true;
+      return false;
     }
     debugPrint("current user is new!!  ");
 
--- apps/mobiles/whilabel/lib/domain/use_case/user_auth/logout_use_case.dart
+++ apps/mobiles/whilabel/lib/domain/use_case/user_auth/logout_use_case.dart
@@ -2,9 +2,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/rendering.dart';
 import 'package:whilabel/data/user/sns_type.dart';
+import 'package:whilabel/domain/global_provider/current_user_state.dart';
 import 'package:whilabel/domain/login_services/googel_oauth.dart';
 import 'package:whilabel/domain/login_services/kakao_oauth.dart';
-import 'package:whilabel/domain/share_provider/current_user_state.dart';
 
 class LogoutUseCase {
   CurrentUserStatus currentUserStatus;
@@ -14,10 +14,10 @@ class LogoutUseCase {
   });
 
   Future<bool> call(SnsType snsType) async {
-    if (currentUserStatus.userState != UserState.login) {
-      debugPrint("정상 로그아웃 실패, 로그인 상태는 유지됨...");
-      return false;
-    }
+    // if (currentUserStatus.userState != UserState.login) {
+    //   debugPrint("정상 로그아웃 실패, 로그인 상태는 유지됨...");
+    //   return false;
+    // }
     switch (snsType) {
       case SnsType.KAKAO:
         await KaKaoOauth().logout();
