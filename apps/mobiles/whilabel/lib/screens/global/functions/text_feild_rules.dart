0dcc71358f34e57e5db9898f6cda14321ed34b5a
hihibara <joungminseong@Joungui-MacBookAir.local>
2023-09-04 20:44:20 +0900

    [feature] Change if conditions
    text_feild_rules.dart안에 사용되는 조건들에 예외 처리 오류를 발견해서
    변동하였습니다.
    
    <오류 예외> (원래라면 에러가 나와야 하는 문자열)
     ex) 이름   :  "성종민@!!@#"  => ok
         닉네임 :  "Joung_Min@@@ " => ok

--- apps/mobiles/whilabel/lib/screens/global/functions/text_feild_rules.dart
+++ apps/mobiles/whilabel/lib/screens/global/functions/text_feild_rules.dart
@@ -1,34 +1,34 @@
 String? checkAbleNameRule(String? value) {
-  if (value == null || value.isEmpty) {
-    return '성함을 입력해주세요';
-  }
-  if (value.toString().length > 10) {
-    return '최대 10자까지 입력 가능';
-  }
-  if (!RegExp(r'[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|ᆞ|ᆢ|ᄀᆞ|ᄂᆞ|ᄃᆞ|ᄅᆞ|ᄆᆞ|ᄇᆞ|ᄉᆞ|ᄋᆞ|ᄌᆞ|ᄎᆞ|ᄏᆞ|ᄐᆞ|ᄑᆞ|ᄒᆞ]')
-      .hasMatch(value)) {
-    return '한글만 입력';
-  }
-  if (RegExp(' ').hasMatch(value)) {
-    return '스페이스는 포함할 수 없습니다';
-  }
+  if (value == null || value.isEmpty) return '한글만 입력가능합니다';
+
+  if (value.toString().length > 10) return '최대 10자까지 입력 가능';
+
+  if (RegExp(r'[!@#%^&*(),.?";:{}|<>-_+=]').hasMatch(value)) return '특수문자 x';
+
+  if (RegExp('[0-9]').hasMatch(value)) return '숫자 입력 x';
+
+  if (RegExp('[a-z | A-Z]').hasMatch(value)) return '영어 입력 x';
+
+  if (RegExp(' ').hasMatch(value)) return '스페이스는 포함할 수 없습니다';
+
+  if (!RegExp('[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|ᆞ|ᆢ|ᄀᆞ|ᄂᆞ|ᄃᆞ|ᄅᆞ|ᄆᆞ|ᄇᆞ|ᄉᆞ|ᄋᆞ|ᄌᆞ|ᄎᆞ|ᄏᆞ|ᄐᆞ|ᄑᆞ|ᄒᆞ]')
+      .hasMatch(value)) return '한글만 입력';
+
   return null;
 }
 
 String? checkAbleNickNameRule(String? value) {
-  if (value == null || value.isEmpty) {
-    return '성함을 입력해주세요';
-  }
-  if (value.toString().length > 15) {
-    return '최대 15자까지 입력 가능';
-  }
+  if (value == null || value.isEmpty) return '[한글, 영어, 숫자, _ ]만 가능합니다';
+
+  if (value.toString().length > 15) return '최대 15자까지 입력 가능';
+
+  if (RegExp(' ').hasMatch(value)) return '스페이스는 포함할 수 없습니다';
+
+  if (RegExp(r'[!@#%^&*(),.?";:{}|<>+=]').hasMatch(value)) return '특수문자 x';
+
   if (!RegExp(
           r'[_|a-z|A-Z|0-9|ㄱ-ㅎ|ㅏ-ㅣ|가-힣|ᆞ|ᆢ|ᄀᆞ|ᄂᆞ|ᄃᆞ|ᄅᆞ|ᄆᆞ|ᄇᆞ|ᄉᆞ|ᄋᆞ|ᄌᆞ|ᄎᆞ|ᄏᆞ|ᄐᆞ|ᄑᆞ|ᄒᆞ]')
-      .hasMatch(value)) {
-    return '[한글, 영어, 숫자, _ ]만 가능합니다';
-  }
-  if (RegExp(' ').hasMatch(value)) {
-    return '스페이스는 포함할 수 없습니다';
-  }
+      .hasMatch(value)) return '[한글, 영어, 숫자, _ ]만 가능합니다';
+
   return null;
 }
