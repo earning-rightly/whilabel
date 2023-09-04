9b846837714946ed7655b2099fb108ec414eee2e
choejong-won <bmsyg987@gmail.com>
2023-09-04 16:34:41 +0900

    extract_to_firebase() 추가

--- apps/batches/wb_libs/lib_firebase.py
+++ apps/batches/wb_libs/lib_firebase.py
@@ -46,3 +46,29 @@ def connect_app():
                                       options={'projectId': 'whilabel'})
     else:  # 이미 firebase에 연결이 되어있는 경우 get_app을 통해 admin생성
         firebase_admin.get_app()
+
+def extract_to_firebase(collection_name: CollectionName) -> dict:
+    """
+        extract_to_firebase.
+            Args:
+               mode : 데이터를 추출해야할 위치 주소 값입니다 (위스키, 증류소, 브랜드 등...)
+
+            Note:
+                fire-base/fire-store에 데이터를 가져오기위한 함수
+
+            Return:
+                dict
+    """
+    start = timeit.default_timer()  # 시작 시간 기록
+
+    connect_app()
+
+    db = firestore.client()  # firesotore client 생성
+    query = db.collection(collection_name.value)
+
+
+    stop = timeit.default_timer()  # 종료 시간 기록
+    print(collection_name.value + " data load to fire store total time taken : " + str(stop - start))  # 총 걸린 시간 출력 (종료 시간 - 시작 시간)
+
+    for doc in query.stream():
+        return doc.to_dict()
