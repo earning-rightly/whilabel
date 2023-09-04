9f0eb294eb14f8134d6e48613e722b9a3ea9730e
choejong-won <bmsyg987@gmail.com>
2023-09-04 17:26:31 +0900

    1. wb_whisky_form -> wb_whisky_pre_form으로 수집된 whisky 전처리 2. batch_type(BatchType), batch_id(BatchExecution) 파리미터 추가 2-1. mode, level파라미터 삭제 3. write_log 주석처리

--- apps/batches/wb_distillery_collector/wb_whisky_belonging_to_distillery_collector/wb_whisky_collector_detail/wb_whisky_detail_belonging_to_distillery_collector_directed_acyclic_execution.py
+++ apps/batches/wb_distillery_collector/wb_whisky_belonging_to_distillery_collector/wb_whisky_collector_detail/wb_whisky_detail_belonging_to_distillery_collector_directed_acyclic_execution.py
@@ -1,13 +1,11 @@
-from apps.batches.wb_libs.enums import CollectionName
-from apps.batches.wb_libs.lib_firebase import save_to_firebase
 from apps.batches.wb_whisky_collector.wb_whisky_collector_detail import wb_whisky_collector_detail_func
 from apps.batches.wb_libs import wb_libs_func
 from apps.batches.wb_whisky_collector.wb_whisky_collector_detail.wb_whisky_collector_detail_values import \
     whisky_collect_detail_scrap as whisky_detail_scrap
-from apps.batches.transformations.wb_whisky.wb_whisky_form import replace_extracted_data_with_wb_whisky_formmat
+from apps.batches.transformations.wb_whisky.wb_whisky_pre_form import replace_extracted_data_with_wb_whisky_format
+from apps.batches.wb_libs.enums import BatchType,BatchExecution
 
-
-def whisky_detail_distillery_collector_executions(mode : str = 'distillery', level : str = 'detail'):
+def whisky_detail_distillery_collector_executions(batch_type : BatchType, batch_id : BatchExecution):
     """
         whisky_detail_distillery_collector_executions.
             Args:
@@ -17,19 +15,19 @@ def whisky_detail_distillery_collector_executions(mode : str = 'distillery', lev
                 scrap_main 에서 위스키 상세정보 스케줄링 함수
     """
 
-    wb_libs_func.write_log(current_time=wb_libs_func.extract_time(), mode=mode+'_whisky', log_mode='start',
-                           level=level)  # 시작 로그 기록
-    wb_whisky_collector_detail_func.collect(mode = mode, current_date=wb_libs_func.extract_time()[0])  # 위스키 사전정보 수집 함수 호출
+    #wb_libs_func.write_log(current_time=wb_libs_func.extract_time(), mode=mode+'_whisky', log_mode='start',
+    # TODO: logger
+        #                   level=level)  # 시작 로그 기록
+    wb_whisky_collector_detail_func.collect(batch_type=BatchType.DISTILLERY_WHISKY_LINK.value,
+                                            current_date=wb_libs_func.extract_time()[0])  # 위스키 사전정보 수집 함수 호출
     wb_libs_func.save_resconvert_csv_to_json(current_date=wb_libs_func.extract_time()[0],
                                              result_dict=whisky_detail_scrap, dir_path='detail/',
-                                             file_form='wb_distillery_whisky_collector_detail')  # 위스키 사전정보 저장 함수 호출
-    wb_libs_func.write_log(current_time=wb_libs_func.extract_time(), mode=mode+'_whisky', log_mode='end', level=level)  # 종료 로그 기록
-    transform_result_list, transform_result_dict = replace_extracted_data_with_wb_whisky_formmat(
-        extract_data=whisky_detail_scrap)
-
+                                             file_form=batch_type.value)  # 위스키 사전정보 저장 함수 호출
+    #wb_libs_func.write_log(current_time=wb_libs_func.extract_time(), mode=mode+'_whisky', log_mode='end', level=level)  # 종료 로그 기록
+    # TODO: logger
+    transform_result_dict = replace_extracted_data_with_wb_whisky_format(extract_data=whisky_detail_scrap,
+                                                                         batchId=batch_id.value)
     wb_libs_func.save_resconvert_csv_to_json(current_date=wb_libs_func.extract_time()[0],
                                              result_dict=transform_result_dict,
                                              dir_path='transformation/',
-                                             file_form='wb_' + mode + '_whiky')  # True)  # 브랜드 사전정보 저장 함수 호출
-
-    save_to_firebase(CollectionName.WHISKY, transform_result_list, 'barcode', 'wbWhisky')
+                                             file_form=batch_type.value)
