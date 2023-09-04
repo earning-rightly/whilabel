ee684bdf7f22ed7ec0c16138e68e714398e14654
hihibara <joungminseong@Joungui-MacBookAir.local>
2023-09-04 16:14:58 +0900

    [feature] Add NickNameAttionalPage()
    - UserInfoAdditionalView를 호출하면 제일 처음  나오는 페이지 입니다.
    - 유저가 입력한 nick name 이 중복이 없고 규칙에 맞는지 확인하는 page

---
+++ apps/mobiles/whilabel/lib/screens/user_info_additional/nick_name_attional_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:whilabel/screens/global/functions/text_feild_rules.dart';
+import 'package:whilabel/screens/global/functions/text_field_styles.dart';
+import 'package:whilabel/screens/user_info_additional/rest_info_additional_page.dart';
+
+class NickNameAttionalPage extends StatefulWidget {
+  const NickNameAttionalPage({super.key});
+
+  @override
+  State<NickNameAttionalPage> createState() => _NickNameAttionalPageState();
+}
+
+class _NickNameAttionalPageState extends State<NickNameAttionalPage> {
+  final nickNameText = TextEditingController();
+  String userNickname = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.close_fullscreen),
+          onPressed: () {},
+        ),
+      ),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              child: Column(
+                children: [
+                  Text(
+                    "닉네임을 설정해주세요",
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  TextFormField(
+                    maxLength: 20,
+                    controller: nickNameText,
+                    decoration: makeWhiskeyRegisterTextFieldStyle(
+                        "한글, 영문, 숫자, 언더바 가능", true),
+                    validator: (value) => checkAbleNickNameRule(value),
+                    onChanged: (value) {
+                      setState(() {
+                        userNickname = value;
+                      });
+                    },
+                  )
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              child: ElevatedButton(
+                child: Text("다음"),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          RestInfoAddtionalPage(nickName: userNickname),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
