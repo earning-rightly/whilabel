031cf6b0e8cd4072c337c1186175062a528c4bbe
choejong-won <bmsyg987@gmail.com>
2023-09-04 17:19:48 +0900

    1. result_df_list.append시 wbId, wbBrand(Distillery, Bottler) 만 초기화 -> 일단 모든 키에 값 초기화 2. 수집된 파일 호출시 f-string 으로 변경 2-1. 파일명 BatchType(enum)으로 검색

--- apps/batches/transformations/wb_distillery/wb_distillery_form.py
+++ apps/batches/transformations/wb_distillery/wb_distillery_form.py
@@ -1,10 +1,11 @@
-from typing import List, Union, Any
+import uuid
+
 import json
 import pandas as pd
+from apps.batches.wb_libs import wb_libs_func
 import numpy as np
 from datetime import datetime
-
-from apps.batches.wb_libs import wb_libs_func
+from apps.batches.wb_libs.enums import BatchType
 
 new_keys_list = ['company_about', 'company_address', 'closed', 'founded', 'owner'
     , 'spirit_stills', 'status','specialists','capacity_per_year','collection','wishlist',
@@ -19,9 +20,6 @@ def make_distillery_id(table: pd.Series) -> int:  # fusion 가능
 
             Note:
                 link주소값에서 증류소 id 값을 추출하여 해당 값 반환
-
-            Return:
-                int
     """
     try:
         return table.link.split('/')[-2]  # 증류소 id 값 추출
@@ -39,9 +37,6 @@ def trans_capacity_per_year(table: pd.Series) -> [int, str]:  # fusion 가능
             Note:
                 한해 생산량 65000 Liters로 표기 되어있어
                 capacity_per_year : 65000, capacity_per_year_unit : Liters로 분리
-
-            Returns:
-                [int, str]
     """
     try:
         split_data = table.capacity_per_year.split(' ')
@@ -59,9 +54,6 @@ def change_specialists_format(table: pd.Series) -> dict:
 
             Note:
                 수집된 스페셜 리스트 포맷을 변환
-
-            Return:
-                dict
     """
     try:
         specialists_dict = eval(table.specialists)  # 스페셜 리스트 형태 dict로 변환
@@ -74,23 +66,12 @@ def change_specialists_format(table: pd.Series) -> dict:
 
 
 def closed_change_datetime_form(table: pd.Series) -> datetime or None:
-    """
-        closed_change_datetime_form
-            Args:
-               table : 변경해야할 판다스 시리스(한 행)
-
-            Note:
-                 closed컬럼 01.01.1991 -> 1991-01-01로 변경
-
-            Return:
-                datetime or None
-    """
-    datetime_format = "%d.%m.%Y" #기존에 포맷 가져오기
+    datetime_format = "%d.%m.%Y"
     try:
-        datetime_result = datetime.strptime(table.closed, datetime_format)#datetime으로 만들기
-        return datetime_result.strftime("%Y-%m-%d")#1991-01-01와같은 포맷으로 다시 만들기
+        datetime_result = datetime.strptime(table.closed, datetime_format)
+        return datetime_result.strftime("%Y-%m-%d")
 
-    except TypeError:# closed컬럼에 값이 없는경우
+    except TypeError:
         return None
 
     except ValueError:
@@ -98,43 +79,25 @@ def closed_change_datetime_form(table: pd.Series) -> datetime or None:
 
 
 def founded_change_datetime_form(table: pd.Series) -> datetime or None:
-    """
-        founded_change_datetime_form
-            Args:
-               table : 변경해야할 판다스 시리스(한 행)
-
-            Note:
-                 founded컬럼 01.01.1991 -> 1991-01-01로 변경
-
-            Return:
-                datetime or None
-    """
-    datetime_format = "%d.%m.%Y" #기존에 포맷 가져오기
+    datetime_format = "%d.%m.%Y"
     try:
-        datetime_result = datetime.strptime(table.founded, datetime_format)#datetime으로 만들기
-        return datetime_result.strftime("%Y-%m-%d")#1991-01-01와같은 포맷으로 다시 만들기
+        datetime_result = datetime.strptime(table.founded, datetime_format)
+        return datetime_result.strftime("%Y-%m-%d")
 
-    except TypeError:  # founded컬럼에 값이 없는경우
+    except TypeError:
         return None
 
 
-def replace_extracted_data_with_wb_distillery_formmat() -> [list, dict]:
+def replace_extracted_data_with_wb_distillery_formmat(batchId : str = None) -> [list, dict]:
     """
         replace_extracted_data_with_wb_distillery_formmat
 
             Note:
-                수집된 bottler 정보를 병합 및 전처리 를 진행하는 함수
-
-            Returns:
-                [list, dict]
+                수집된 증류소 정보를 병합 및 전처리 를 진행하는 함수
     """
     # 수집된 파일 가져오기 : 파일 주소가 아닌 파라미터로 넘기는게 어떤지...?
-    distillery_detail_data = pd.read_csv(
-        "/Users/choejong-won/PycharmProjects/whiskey_search/batches/results/" + wb_libs_func.extract_time()[
-            0] + '/csv/detail/wb_distillery_collector_detail.csv')
-    distillery_summary_data = pd.read_csv(
-        "/Users/choejong-won/PycharmProjects/whiskey_search/batches/results/" + wb_libs_func.extract_time()[
-            0] + '/csv/pre/wb_distillery_collector_pre.csv')
+    distillery_detail_data = pd.read_csv(f'/Users/choejong-won/PycharmProjects/whilabel/apps/batches/results/{wb_libs_func.extract_time()[0]}/csv/detail/{BatchType.DISTILLERY_DETAIL.value}.csv')
+    distillery_summary_data = pd.read_csv(f'/Users/choejong-won/PycharmProjects/whilabel/apps/batches/results/{wb_libs_func.extract_time()[0]}/csv/pre/{BatchType.DISTILLERY_PRE.value}.csv')
 
     distillery_detail_data_remove_columns = distillery_detail_data.loc[:, new_keys_list]  # 불필요한 컬럼 제거
     result_df = pd.merge(distillery_summary_data,
@@ -144,34 +107,38 @@ def replace_extracted_data_with_wb_distillery_formmat() -> [list, dict]:
                          right_index=True)
     result_df['wb_distillery_id'] = result_df.apply(make_distillery_id, axis=1)  # 증류소 id 처리
     result_df[['capacity_per_year', 'capacity_per_year_unit']] = result_df.apply(trans_capacity_per_year, axis=1,   result_type='expand')  # 한해 생산량 전처리
-    result_df['closed'] = result_df.apply(closed_change_datetime_form, axis=1)  # closed 처리
-    result_df['founded'] = result_df.apply(founded_change_datetime_form, axis=1)  # founded 처리
+    result_df['closed'] = result_df.apply(closed_change_datetime_form, axis=1)  # 스페셜리스트 처리
+    result_df['founded'] = result_df.apply(founded_change_datetime_form, axis=1)  # 스페셜리스트 처리
     result_df['specialists'] = result_df.apply(change_specialists_format, axis=1)  # 스페셜리스트 처리
     result_df['batchedAt'] = wb_libs_func.extract_time()[1]
-    result_df['batchId'] = 'wb_distillery'
+    result_df['batchId'] = batchId
 
-    result_df = result_df[['wb_distillery_id', 'distillery_name', 'whiskies', 'votes', 'rating', 'link', 'company_about',
-         'company_address', 'capacity_per_year', 'capacity_per_year_unit', 'closed', 'collection', 'country', 'founded',
-         'owner', 'specialists'
-            , 'spirit_stills', 'status', 'views', 'wb_ranking', 'website', 'wishlist','batchedAt','batchId']]
+    result_df = result_df[['wb_distillery_id', 'distillery_name', 'whiskies', 'votes', 'rating', 'link',
+                            'company_address', 'closed', 'country', 'founded','wash_stills','owner', 'spirit_stills', 'status',
+                           'views', 'wb_ranking', 'website' ,'batchedAt','batchId']]
+
+    result_df.rename(columns={'wb_distillery_id': 'wbId', 'distillery_name': 'name', 'whiskies': 'whiskyCount',
+                              'votes': 'voteCount','link' : 'wbLink', 'website' : 'link','spirit_stills' : 'spiritStills',
+                              'views' : 'viewCount','wb_ranking' :'wbRanking','wash_stills':'washStills','company_address' : 'address'}, inplace=True)
 
     result_df = result_df.replace({np.nan: None})  # json으로 저장하기 위해 np.nan -> null로 변환
-    result_df_list = []   #fire store에 저장하기 위해 각 로우별로 json화 시켜 라스트에 저장
-    for i in range(len(result_df)):
+    result_df_list = []
+    for result_index in range(len(result_df)):
         result_df_list.append({
-            'id': None,
-            'createdAt': None,
+            'id': str(uuid.uuid5(uuid.NAMESPACE_URL, result_df.wbLink[result_index])), #네임 스페이스 UUID와 이름의 SHA-1 해시에서 UUID를 생성합니다.,
+            'createdAt': wb_libs_func.extract_time()[1],
             'creator': None,
-            'modifiedAt': None,
+            'modifiedAt': wb_libs_func.extract_time()[1],
             'modifier': None,
-            'wbId': result_df.wb_distillery_id[i],
-            'name': result_df.distillery_name[i],
-            'country': result_df.country[i],
-            'link': result_df.link[i],
-            'wbDistillery': json.loads(result_df.loc[i, :].to_json()),
-            'tasteFeature' : None
+            'name': result_df.name[result_index],
+            'country': result_df.country[result_index],
+            'link': result_df.link[result_index],
+            'tasteFeature': None, #보류
+            'wbId': result_df.wbId[result_index],
+            'wbDistillery': json.loads(result_df.loc[result_index, :].to_json()),
+
 
         })
-    return result_df_list, json.loads(result_df.to_json())  #[파이어베이스 저장포맷,  (csv, json) 저장포맷]
+    return result_df_list, json.loads(result_df.to_json())
 
 
