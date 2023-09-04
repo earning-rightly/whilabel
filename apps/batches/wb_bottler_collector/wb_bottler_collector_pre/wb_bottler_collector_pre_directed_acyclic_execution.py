fe019f9c7ffc3203047a238ec4b54f85040a4444
choejong-won <bmsyg987@gmail.com>
2023-09-04 17:33:55 +0900

    1. write_log 주석처리 2. batch_type(BatchType), batch_id(BatchExecution) 파리미터 추가 2-1. mode, level파라미터 삭제

--- apps/batches/wb_bottler_collector/wb_bottler_collector_pre/wb_bottler_collector_pre_directed_acyclic_execution.py
+++ apps/batches/wb_bottler_collector/wb_bottler_collector_pre/wb_bottler_collector_pre_directed_acyclic_execution.py
@@ -2,9 +2,9 @@ from apps.batches.wb_bottler_collector.wb_bottler_collector_pre import wb_bottle
 from apps.batches.wb_bottler_collector.wb_bottler_collector_pre.wb_bottler_collector_pre_values import \
     bottler_summary_collect_data as pre_scrap
 from apps.batches.wb_libs import wb_libs_func
+from apps.batches.wb_libs.enums import BatchType
 
-
-def bottler_pre_executions(mode='bottler', level='pre'):
+def bottler_pre_executions(batch_type : BatchType):
     """
        bottler_pre_executions.
            Args:
@@ -13,17 +13,11 @@ def bottler_pre_executions(mode='bottler', level='pre'):
            Note:
                scrap_main 에서 bottler 사전 정보 스케줄링 함수
     """
-    wb_libs_func.write_log(current_time=wb_libs_func.extract_time(),
-                           log_mode='start',
-                           mode=mode,
-                           level=level)  # 시작 로그 기록
+    #wb_libs_func.write_log(current_time=wb_libs_func.extract_time(),log_mode='start',mode=mode,level=level)  # 시작 로그 기록
     wb_bottler_collector_pre_func.collect()  # 증류소 사전정보 수집 함수 호출
     wb_libs_func.save_resconvert_csv_to_json(current_date=wb_libs_func.extract_time()[0],
                                              result_dict=pre_scrap,
                                              dir_path='pre/',
-                                             file_form='wb_bottler_collector_pre')  # 증류소 사전정보 저장 함수 호출
-    wb_libs_func.write_log(current_time=wb_libs_func.extract_time(),
-                           log_mode='end',
-                           mode=mode,
-                           level=level)  # 종료 로그 기록
+                                             file_form=batch_type.value)  # 증류소 사전정보 저장 함수 호출
+    #wb_libs_func.write_log(current_time=wb_libs_func.extract_time(),log_mode='end', mode=mode,level=level)  # 종료 로그 기록
 
--- apps/batches/wb_distillery_collector/wb_distillery_collector_pre/wb_distillery_collector_pre_directed_acyclic_execution.py
+++ apps/batches/wb_distillery_collector/wb_distillery_collector_pre/wb_distillery_collector_pre_directed_acyclic_execution.py
@@ -2,9 +2,9 @@ from apps.batches.wb_distillery_collector.wb_distillery_collector_pre import wb_
 from apps.batches.wb_distillery_collector.wb_distillery_collector_pre.wb_distillery_collector_pre_values import \
     distillery_summary_collect_data as pre_scrap
 from apps.batches.wb_libs import wb_libs_func
+from apps.batches.wb_libs.enums import BatchType
 
-
-def distillery_pre_executions(mode : str ='distillery', level : str ='pre'):
+def distillery_pre_executions(batch_type : BatchType):
     """
        distillery_pre_executions.
            Args:
@@ -13,17 +13,11 @@ def distillery_pre_executions(mode : str ='distillery', level : str ='pre'):
            Note:
                scrap_main 에서 증류소 사전 정보 스케줄링 함수
     """
-    wb_libs_func.write_log(current_time=wb_libs_func.extract_time(),
-                           log_mode='start',
-                           mode=mode,
-                           level=level)  # 시작 로그 기록
+    #wb_libs_func.write_log(current_time=wb_libs_func.extract_time(), log_mode='start',mode=batch_type,level=level)  # 시작 로그 기록
     wb_distillery_collector_pre_func.collect()  # 증류소 사전정보 수집 함수 호출
     wb_libs_func.save_resconvert_csv_to_json(current_date=wb_libs_func.extract_time()[0],
                                              result_dict=pre_scrap,
                                              dir_path='pre/',
-                                             file_form='wb_distillery_collector_pre')  # 증류소 사전정보 저장 함수 호출
-    wb_libs_func.write_log(current_time=wb_libs_func.extract_time(),
-                           log_mode='end',
-                           mode=mode,
-                           level=level)  # 종료 로그 기록
+                                             file_form=batch_type.value)  # 증류소 사전정보 저장 함수 호출
+    #wb_libs_func.write_log(current_time=wb_libs_func.extract_time(),log_mode='end',mode=mode,level=level)  # 종료 로그 기록
 
