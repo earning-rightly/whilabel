40714d7fb13190428bb4705552b7f27436c61695
2821char <109503403+hihibara@users.noreply.github.com>
2023-09-09 01:12:18 +0900

    [Feature] 마이페이지에서 사용할 widgets 추가 (#112)
    * [feature] Add ListTitleIconButton() widget
    
    MyPageView()에서 왼쪽에 아이콘, 오른쪽에 text가 있는 리스버튼 입니다.
    
    * [feature] Add ListToggleButton()
    
    SettingPage()에서 알림 on/off를 해주는 widget입니다.
    
    * [feature] Add ShortWhiskeyCounter()
    
    MyPageView()에서 유저에게 등록한 위스키와 브랜드 갯수를 보여줍니다
    
    * [fix] showLogoutDialog  & showWithdrawalDialog error
    
    * [refactory] change widget's parameter
    
    ---------
    
    Co-authored-by: hihibara <joungminseong@Joungui-MacBookAir.local>

---
+++ apps/mobiles/whilabel/lib/screens/my_page/widgets/list_toggel_button.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:whilabel/screens/constants/colors_manager.dart';
+import 'package:whilabel/screens/constants/path/svg_icon_paths.dart';
+import 'package:whilabel/screens/constants/text_styles_manager.dart';
+
+// ignore: must_be_immutable
+class ListToggleButton extends StatelessWidget {
+  ListToggleButton({
+    super.key,
+    required this.title,
+    required this.isToggleState,
+    this.onPressedButton,
+  });
+  final String title;
+  final bool isToggleState;
+  final Function()? onPressedButton;
+
+  String offToggleIconPath = SvgIconPath.toggleOff;
+  String onToggleIconPath = SvgIconPath.toggleOn;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: TextStylesManager().createHadColorTextStyle(
+              "M16",
+              ColorsManager.gray200,
+            ),
+          ),
+          IconButton(
+            iconSize: 70,
+            onPressed: onPressedButton,
+            icon: SvgPicture.asset(
+                isToggleState == true ? onToggleIconPath : offToggleIconPath),
+          )
+        ],
+      ),
+    );
+  }
+}
