4cae70f5a7c7ae31e7e4befb3b481ac60733ab66
2821char <109503403+hihibara@users.noreply.github.com>
2023-09-08 14:36:12 +0900

    [Feature]  User Additional Info View에서 사용할 view model 추가 (#104)
    * [feature] Add UserInfoAdditionalState()
    
    UserInfoAdditionalViewModel을 실행하면서 필요하거나 view에서 확인을
    해야할 정보가 무엇인지 결정하여 만든 데이터 모델입니다.
    
    * [feature] Add UserInfoAdditionalEvent()
    
    view에서 실행할 event를 정의하는 곳입니다. view에서는 event에 접근을
    하고 view model에서 정의한 함수를 실행 시킵니다
    
    * [feature] Add UserInfoAdditionalViewModel()
    
    - UserInfoAdditionalView의 로직을 실행시키는 역활을 하는 provider입니다.
    - UserInfoAdditionalView에서 실행되는 기능은 전부 여기서 실행됩니다.
    - onEvent에 addUserInfo(), checkNickName() 함수를 등록하여 사용합니다
    - _checkContainedForbiddenWord()은 database에 있는 금지어가 닉네임에
      있는지 확인해주는 역활을 수행합니다
    
    * [refactory] user_info_additional => user_additional_info
    
    * [refactory] del user_info_additional
    
    * [Feature] add  isExistedNickName() on AppUserRepository() (#105)
    
    * [feature] Add isExistedNickName() 함수
    
    - database user collection안에서 nick name filed에 파라미터로 받은 nick
    name이 있는지 확인합니다.
    - 반환 값이 ture면 유저가 입력한 nick name 이 존재한다는 것이고 없다면
      false를 반환합니다
    
    * [refactory] 오타 수정nickNaem => nickName
    
    ---------
    
    Co-authored-by: hihibara <joungminseong@Joungui-MacBookAir.local>
    
    ---------
    
    Co-authored-by: hihibara <joungminseong@Joungui-MacBookAir.local>

--- apps/mobiles/whilabel/lib/screens/user_additional_info/user_additional_info_view.dart
+++ apps/mobiles/whilabel/lib/screens/user_additional_info/user_additional_info_view.dart
@@ -7,8 +7,8 @@ import 'package:whilabel/screens/user_info_additional/nick_name_attional_page.da
 
 // UserInfoAdditionalView의 view model과 이름이 햇갈리지 읺기 위해서
 // 추가 정보 페이지를 my 페이지에서 유저 정보 수정에서 사용할 가능성 있음
-class UserInfoAdditionalView extends StatelessWidget {
-  const UserInfoAdditionalView({super.key});
+class UserAdditionalInfoView extends StatelessWidget {
+  const UserAdditionalInfoView({super.key});
 
   @override
   Widget build(BuildContext context) {
