71b645a10347fb425591b1d5367561348b3700af
2821char <109503403+hihibara@users.noreply.github.com>
2023-09-08 14:34:12 +0900

    Feature/add show dialog.dart (#108)
    * [feature] Add PopUpYesOrNoButton on global
    
    showDialog()를 사용할 widget입니다. 두 개 버튼을 주고 선택을 주는
    dialog를 생성합니다.
    
    * [feature] Add show_dialog.dart
    
     MyPage안에서 dialog를 띄어야 할때 사용되는 함수를 저장한 파일입니다.
    
     <파일안에 있는 함수>
    - showLogoutDialog(): 로그아웃 여부를 묻습니다.
    - showWithdrawalDialog() : 회원 탈퇴 여부를 묻습니다
    
    * [refactory] LongTextButton TextStylesManager path
    
    * [refactory] change PopUpYesOrNoButton's parameter name
    
    clickYesFunc  => onClickYesFunc
    clickNoFunc  => onClickNoFunc
    
    * [refactory] move pop_up_yes_no_button.dart on global/
    
    * [reafactory] change parameter name
    
    onClickYesFunc => onClickYesButton
    onClickNoFunc => onClickNoButton
    
    ---------
    
    Co-authored-by: hihibara <joungminseong@Joungui-MacBookAir.local>

---
+++ apps/mobiles/whilabel/lib/screens/global/widgets/pop_up_yes_no_button.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:whilabel/screens/constants/colors_manager.dart';
+import 'package:whilabel/screens/constants/text_styles_manager.dart';
+import 'package:whilabel/screens/constants/whilabel_design_setting.dart';
+
+// 예, 아니오 두 가지 경우로 구성 되는 팝업
+class PopUpYesOrNoButton extends StatelessWidget {
+  final String titleText;
+  final String yesText;
+  final Color? yesButtonColor;
+  final Function()? onClickYesButton;
+
+  final String noText;
+  final Color? noButtonColor;
+  final Function()? onClickNoButton;
+
+  PopUpYesOrNoButton({
+    super.key,
+    required this.titleText,
+    required this.yesText,
+    this.yesButtonColor,
+    this.onClickYesButton,
+    required this.noText,
+    this.noButtonColor,
+    this.onClickNoButton,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      contentPadding: EdgeInsets.zero, // 디폴트 값이 존재하므로 0으로 만들어서 넗혀주기
+      insetPadding: EdgeInsets.symmetric(horizontal: 5),
+      backgroundColor: ColorsManager.black200,
+
+      title: Container(
+        width: 300, // AlertDialog의 width를 넓히기 위해서 사용
+        child: Text(
+          titleText,
+          textAlign: TextAlign.center,
+        ),
+      ),
+      titleTextStyle: TextStylesManager()
+          .createHadColorTextStyle("B16", ColorsManager.gray500),
+      shape: RoundedRectangleBorder(
+          borderRadius:
+              BorderRadius.all(Radius.circular(WhilabelRadius.radius16))),
+      actions: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // 1. No
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 10, left: 15, right: 5),
+                child: ElevatedButton(
+                  onPressed: onClickNoButton,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: noButtonColor ?? ColorsManager.black300,
+                    foregroundColor: ColorsManager.gray500,
+                    fixedSize: Size(120, 50),
+                    shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(WhilabelRadius.radius12)),
+                    side: BorderSide(
+                      width: 1.0,
+                      color: ColorsManager.black200,
+                    ),
+                  ),
+                  child: Text(
+                    noText,
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+            // 2. Yes
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 10, left: 5, right: 15),
+                child: ElevatedButton(
+                  onPressed: onClickYesButton,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: yesButtonColor ?? ColorsManager.brown100,
+                    foregroundColor: ColorsManager.gray500,
+                    fixedSize: Size(120, 50),
+                    shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(WhilabelRadius.radius12)),
+                    side: BorderSide(
+                      width: 1.0,
+                      color: ColorsManager.black200,
+                    ),
+                  ),
+                  child: Text(
+                    yesText,
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
---
+++ apps/mobiles/whilabel/lib/screens/my_page/widgets/functions/show_dialog.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:whilabel/screens/constants/colors_manager.dart';
+import 'package:whilabel/screens/home/grid/widgets/pop_up_yes_no_button.dart';
+
+void showLogoutDialog(BuildContext context, {Function()? clickedYesFuc}) {
+  final String title = "정말로 로그아웃 하시겠어요?";
+
+  showDialog(
+    context: context,
+    // 주의) builder parameter에 있는 context 객체를 사용하면 Provider 객체를 불러올 수 없음.
+    builder: (BuildContext _) => PopUpYesOrNoButton(
+      titleText: title,
+      noText: "취소",
+      yesText: "로그아웃",
+      onClickYesFunc: clickedYesFuc,
+      onClickNoFunc: () {
+        Navigator.pop(context);
+      },
+    ),
+  );
+}
+
+void showWithdrawalDialog(BuildContext context, {Function()? clickedYesFuc}) {
+  final String title = "정말로 탈퇴하시겠어요?";
+
+  showDialog(
+      context: context,
+
+      // 주의) builder parameter에 있는 context 객체를 사용하면 Provider 객체를 불러올 수 없음.
+      builder: (BuildContext _) => PopUpYesOrNoButton(
+            titleText: title,
+            noText: "취소",
+            yesText: "탈퇴하기",
+            yesButtonColor: ColorsManager.red,
+            onClickYesFunc: clickedYesFuc,
+            onClickNoFunc: () {
+              Navigator.pop(context);
+            },
+          ));
+}
