5785ead0307947b7b82c024145ca188eff91dbd0
hihibara <joungminseong@Joungui-MacBookAir.local>
2023-09-08 14:44:26 +0900

    [feature] Add ListTitleIconButton() widget
    MyPageView()에서 왼쪽에 아이콘, 오른쪽에 text가 있는 리스버튼 입니다.

---
+++ apps/mobiles/whilabel/lib/screens/my_page/widgets/list_button.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:whilabel/screens/constants/colors_manager.dart';
+import 'package:whilabel/screens/constants/text_styles_manager.dart';
+import 'package:whilabel/screens/constants/whilabel_design_setting.dart';
+
+class ListTitleIconButton extends StatelessWidget {
+  const ListTitleIconButton({
+    super.key,
+    this.onTabFunc,
+    this.spacing = 12,
+    required this.svgPath,
+    required this.titleText,
+  });
+  final String svgPath;
+  final String titleText;
+  final Function()? onTabFunc;
+  final double spacing;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: WhilabelSpacing.spac12),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SvgPicture.asset(
+              svgPath,
+              height: 25,
+              colorFilter:
+                  ColorFilter.mode(ColorsManager.black400, BlendMode.srcIn),
+            ),
+            SizedBox(
+              width: spacing,
+            ),
+            Text(
+              titleText,
+              style: TextStylesManager.medium16,
+            ),
+          ],
+        ),
+      ),
+      onTap: onTabFunc ?? () {},
+    );
+  }
+}
