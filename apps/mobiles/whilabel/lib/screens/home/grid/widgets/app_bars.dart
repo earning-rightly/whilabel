97493799f8c0e191b378a0e8ff07fe413b6dc944
2821char <109503403+hihibara@users.noreply.github.com>
2023-09-09 12:44:30 +0900

    [feture] Add app_bars.dart on global/widgets/ (#116)
    - 매번 AppBar가 필요한 페이지에서 새로 만들 필용 없이 사용하기 위해서
    - scaffold의 appBar 파라미터에 사용되는 모듈 하나와 body 에서 사용되는
      모듈로 나누어서 사용
    
    Co-authored-by: hihibara <joungminseong@Joungui-MacBookAir.local>

---
+++ apps/mobiles/whilabel/lib/screens/home/grid/widgets/app_bars.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:whilabel/screens/constants/colors_manager.dart';
+import 'package:whilabel/screens/constants/text_styles_manager.dart';
+
+// scaffold의 appBar 파라미터는 AppBar타입만 들어갈 수 있기에 함수로 생성
+AppBar createScaffoldAppBar(
+    BuildContext context, String svgPath, String title) {
+  return AppBar(
+    leading: IconButton(
+      padding: EdgeInsets.zero,
+      alignment: Alignment.centerLeft,
+      icon: SvgPicture.asset(svgPath),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    ),
+    title: Text(
+      title,
+      textAlign: TextAlign.center,
+      style: TextStylesManager().createHadColorTextStyle(
+        "B16",
+        ColorsManager.gray500,
+      ),
+    ),
+  );
+}
+
+class BodyAppBar extends StatelessWidget {
+  const BodyAppBar({
+    super.key,
+    required this.svgPath,
+    required this.centerTitle,
+    this.rightTitle,
+  });
+  final String svgPath;
+  final String centerTitle;
+  final String? rightTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Row(
+        children: [
+          Expanded(
+            child: SvgPicture.asset(svgPath),
+          ),
+          Expanded(
+            child: Text(
+              centerTitle,
+              textAlign: TextAlign.center,
+              style: TextStylesManager().createHadColorTextStyle(
+                "B16",
+                ColorsManager.gray500,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Text(
+              rightTitle ?? "",
+              textAlign: TextAlign.center,
+              style: TextStylesManager().createHadColorTextStyle(
+                "B16",
+                ColorsManager.gray500,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
