45abdc4447c4c42c65c464adeeac150897fa38c1
choejong-won <bmsyg987@gmail.com>
2023-09-09 20:42:34 +0900

    remove unused global var file

--- apps/batches/wb_brand_collector/wb_brand_collector_detail/wb_brand_collector_detail_values.py
+++
@@ -1,6 +0,0 @@
-keys_list = ['country', 'website', 'region']  # 브랜드 상세 정보에서 수집할 컬럼 리스트
-
-brand_collect_detail_scrap = dict.fromkeys(keys_list)  # 브랜드 상세 정보에서 수집할 컬럼 리스트
-
-for key in keys_list:  # 딕셔너리 키별 값(value) 초기화
-    brand_collect_detail_scrap[key] = []
