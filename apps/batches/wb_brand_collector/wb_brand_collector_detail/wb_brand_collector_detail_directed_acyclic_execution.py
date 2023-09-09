c3ea41a88aeeb9808f7acb0123ecf4e01e496c63
choejong-won <bmsyg987@gmail.com>
2023-09-09 20:39:33 +0900

    replace global var to local var

--- apps/batches/wb_brand_collector/wb_brand_collector_detail/wb_brand_collector_detail_directed_acyclic_execution.py
+++ apps/batches/wb_brand_collector/wb_brand_collector_detail/wb_brand_collector_detail_directed_acyclic_execution.py
@@ -5,6 +5,7 @@ from apps.batches.transformations.wb_brand.wb_brand_form import replace_extracte
 from apps.batches.wb_libs.enums import BatchType,CollectionName,BatchExecution
 from apps.batches.wb_libs.lib_firebase import save_to_firebase
 from apps.batches.wb_libs import wb_libs_func
+from apps.batches.wb_libs.constants import detail_field_map
 
 def brand_detail_executions(batch_type : BatchType, batch_execution :  BatchExecution):  # mode allows you to choose between distilleries and brands to collect { mode : distillery, brand} * default = distillery
     """
@@ -16,18 +17,20 @@ def brand_detail_executions(batch_type : BatchType, batch_execution :  BatchExec
                 scrap_main 에서 브랜드 상세정보 스케줄링 함수
     """
 
+    scrap_dict = wb_libs_func.initialize_dict(detail_field_map['wb_brand_collect_detail'])
+
     current_date = wb_libs_func.get_current_date()
 
-    wb_brand_collector_detail_func.collect(current_date = current_date)  # 브랜드 상세정보 수집 함수 호출
+    wb_brand_collector_detail_func.collect(batch_type.BRAND_PRE.value, current_date, scrap_dict)  # 브랜드 상세정보 수집 함수 호출
 
     # save as csv file
-    result_df = wb_libs_func.convert_to_df(detail_scrap)
+    result_df = wb_libs_func.convert_to_df(scrap_dict)
     csv_path = f'results/{current_date}/csv/detail/'
     wb_libs_func.save_to_csv(result_df, csv_path, batch_type.value)
 
     # save as json file
     json_path = f'results/{current_date}/json/detail/'
-    wb_libs_func.save_to_json(detail_scrap, json_path, batch_type.value)
+    wb_libs_func.save_to_json(scrap_dict, json_path, batch_type.value)
 
     transform_result_list, transform_result_dict = replace_extracted_data_with_wb_brand_formmat(batchId=batch_execution.value)  # 수집된 브랜드 정보 전처리
 
