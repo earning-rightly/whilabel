2b20c4891ee2640dc6d22c3eba66882942b3869c
kor-jongwon <bmsyg987@gmail.com>
2023-09-10 23:23:16 +0900

    [refactory][batch] 2. refactoring distillery detail global values (#119)
    * add wb_distillery_collect_detail
    
    * replace global var to local var
    
    * replace global var to local var
    
    * remove unused global var import
    
    * 커밋 변경

--- apps/batches/wb_distillery_collector/wb_distillery_collector_detail/wb_distillery_collector_detail_directed_acyclic_execution.py
+++ apps/batches/wb_distillery_collector/wb_distillery_collector_detail/wb_distillery_collector_detail_directed_acyclic_execution.py
@@ -1,13 +1,13 @@
 from apps.batches.wb_libs import wb_libs_func
 from apps.batches.wb_distillery_collector.wb_distillery_collector_detail import wb_distillery_collector_detail_func
-from apps.batches.wb_distillery_collector.wb_distillery_collector_detail.wb_distillery_collector_detail_values import \
-    distillery_collect_detail_scrap as detail_scrap
 from apps.batches.transformations.wb_distillery.wb_distillery_form import \
     replace_extracted_data_with_wb_distillery_formmat
-from apps.batches.wb_libs.enums import CollectionName,BatchType,BatchExecution
+from apps.batches.wb_libs.enums import CollectionName, BatchType, BatchExecution
 from apps.batches.wb_libs.lib_firebase import save_to_firebase
+from apps.batches.wb_libs.constants import detail_field_map
 
-def distillery_detail_executions(batch_type : BatchType, batch_execution : BatchExecution):
+
+def distillery_detail_executions(batch_type: BatchType, batch_execution: BatchExecution):
     """
         distillery_detail_executions.
             Args:
@@ -17,21 +17,27 @@ def distillery_detail_executions(batch_type : BatchType, batch_execution : Batch
                 scrap_main 에서 증류소 상세정보 스케줄링 함수
     """
 
+    scrap_dict = wb_libs_func.initialize_dict(detail_field_map['wb_distillery_collect_detail'])
+
     current_date = wb_libs_func.get_current_date()
 
-    wb_distillery_collector_detail_func.collect(current_date=current_date)  # 증류소 상세정보 수집 함수 호출
+    wb_distillery_collector_detail_func.collect(batch_type.DISTILLERY_PRE.value, current_date,
+                                                scrap_dict)  # 증류소 상세정보 수집 함수 호출
+    wb_distillery_collector_detail_func.collect(batch_type.DISTILLERY_PRE.value, current_date,
+                                                scrap_dict)  # 증류소 상세정보 수집 함수 호출
 
     # save as csv file
-    result_df = wb_libs_func.convert_to_df(detail_scrap)
+    result_df = wb_libs_func.convert_to_df(scrap_dict)
     csv_path = f'results/{current_date}/csv/detail/'
     wb_libs_func.save_to_csv(result_df, csv_path, batch_type.value)
 
     # save as json file
     json_path = f'results/{current_date}/json/detail/'
-    wb_libs_func.save_to_json(detail_scrap, json_path, batch_type.value)
+    wb_libs_func.save_to_json(scrap_dict, json_path, batch_type.value)
 
-    transform_result_list, transform_result_dict = replace_extracted_data_with_wb_distillery_formmat(batchId=batch_execution.value)
-    
+    transform_result_list, transform_result_dict = (replace_extracted_data_with_wb_distillery_formmat
+                                                    (batchId=batch_execution.value))
+
     # save as csv file
     result_df = wb_libs_func.convert_to_df(transform_result_dict)
     csv_path = f'results/{current_date}/csv/transformation/'
