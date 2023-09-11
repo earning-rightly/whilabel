7fed156c950e1468f9eca3804e7b35d5b436f924
choejong-won <bmsyg987@gmail.com>
2023-09-11 10:46:22 +0900

    change constans.py to field_map and modify the pre file

--- apps/batches/wb_brand_collector/wb_brand_collector_pre/wb_brand_collector_pre_directed_acyclic_execution.py
+++ apps/batches/wb_brand_collector/wb_brand_collector_pre/wb_brand_collector_pre_directed_acyclic_execution.py
@@ -1,8 +1,8 @@
 from apps.batches.wb_brand_collector.wb_brand_collector_pre import wb_brand_collector_pre_func
 from apps.batches.wb_libs import wb_libs_func
 from apps.batches.wb_libs.enums import BatchType
-from apps.batches.wb_libs.constants import pre_field_map
-from apps.batches.wb_libs.constants import pre_link_map
+from apps.batches.wb_libs.constants import field_map
+from apps.batches.wb_libs.constants import link_map
 def brand_pre_executions(batch_type : BatchType):
     """
        brand_pre_executions.
@@ -13,9 +13,9 @@ def brand_pre_executions(batch_type : BatchType):
                scrap_main 에서 브랜드 사전 정보 스케줄링 함수
     """
 
-    scrap_dict = wb_libs_func.initialize_dict(pre_field_map['wb_brand_collect_pre'])
+    scrap_dict = wb_libs_func.initialize_dict(field_map['wb_brand_collect_pre'])
 
-    wb_brand_collector_pre_func.collect(scrap_dict, pre_link_map['brand'])  # 증류소 사전정보 수집 함수 호출
+    wb_brand_collector_pre_func.collect(scrap_dict, link_map['brand'])  # 증류소 사전정보 수집 함수 호출
 
     current_date = wb_libs_func.get_current_date()
 
