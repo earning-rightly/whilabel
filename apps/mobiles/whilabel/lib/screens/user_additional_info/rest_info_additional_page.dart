c5a4fcd2d79d656c2dbb3d720e90f0b83ec7b10f
hihibara <joungminseong@Joungui-MacBookAir.local>
2023-09-11 16:27:31 +0900

    [feature] Add UserAdditionalInfo's pages design
    - NickNameAttionalPage() 에 피그마 디자인을 추가합니다
    - RestInfoAddtionalPage()에 피그마 디자인을 추가합니다

--- apps/mobiles/whilabel/lib/screens/user_additional_info/rest_info_additional_page.dart
+++ apps/mobiles/whilabel/lib/screens/user_additional_info/rest_info_additional_page.dart
@@ -4,13 +4,18 @@ import 'package:provider/provider.dart';
 import 'package:whilabel/data/user/app_user.dart';
 import 'package:whilabel/data/user/enum/gender.dart';
 import 'package:whilabel/domain/global_provider/current_user_state.dart';
+import 'package:whilabel/screens/constants/colors_manager.dart';
 import 'package:whilabel/screens/constants/routes_manager.dart';
+import 'package:whilabel/screens/constants/text_styles_manager.dart';
+import 'package:whilabel/screens/constants/whilabel_design_setting.dart';
+import 'package:whilabel/screens/global/functions/text_feild_rules.dart';
 import 'package:whilabel/screens/global/widgets/loding_progress_indicator.dart';
+import 'package:whilabel/screens/global/widgets/long_text_button.dart';
 import 'package:whilabel/screens/user_additional_info/view_model/user_additional_info_event.dart';
 import 'package:whilabel/screens/user_additional_info/view_model/user_additional_info_view_model.dart';
+import 'package:whilabel/screens/user_additional_info/widget/gender_choicer.dart';
 import 'package:whilabel/screens/user_additional_info/widget/user_birth_date_picker.dart';
 import 'package:whilabel/screens/user_additional_info/widget/user_name_input_text_field.dart';
-import 'package:whilabel/screens/user_additional_info/widget/gender_choicer.dart';
 
 // ignore: must_be_immutable
 class RestInfoAddtionalPage extends StatefulWidget {
@@ -32,6 +37,7 @@ class _RestInfoAddtionalPageState extends State<RestInfoAddtionalPage> {
   final _formKey = GlobalKey<FormState>();
   final nameTextController = TextEditingController();
   final birthDayTextController = TextEditingController();
+
   bool isfilledAllData = false;
 
   @override
@@ -51,7 +57,8 @@ class _RestInfoAddtionalPageState extends State<RestInfoAddtionalPage> {
           }
           AppUser currentUser = snapshot.data!;
 
-          return SafeArea(
+          return Padding(
+            padding: WhilablePadding.basicPadding,
             child: Form(
               key: _formKey,
               autovalidateMode: AutovalidateMode.onUserInteraction,
@@ -63,50 +70,81 @@ class _RestInfoAddtionalPageState extends State<RestInfoAddtionalPage> {
                   });
                 });
               },
-              child: Column(
-                children: [
-                  Text(
-                    "${widget.nickName}님에 대해서 조금만 더 알려주세요",
-                    style: TextStyle(color: Colors.white, fontSize: 28),
-                  ),
-                  // 이름 입력
-                  UserNameInputTextField(
-                    nameTextController: nameTextController,
-                  ),
-                  // 성별 선택
-                  GenderChoicer(
-                    isFemalePressed: widget.isFemalePressed,
-                    isManPressed: widget.isManPressed,
-                    onPressedFemale: onPressedFemaleButton,
-                    onPressedMan: onPressedManButton,
-                  ),
-                  UserBirthDatePicker(
-                    birthDayTextController: birthDayTextController,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
+              child: SafeArea(
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 0,
+                      right: 0,
+                      left: 0,
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.height,
+                        height: MediaQuery.of(context).size.height / 2,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            // 유저 닉네임 보여주는 widgeet
+                            Flexible(
+                              child: Padding(
+                                padding: const EdgeInsets.only(bottom: 32),
+                                child: Text(
+                                  "${widget.nickName}님에 대해서\n조금만 더 알려주세요",
+                                  style: TextStylesManager()
+                                      .createHadColorTextStyle(
+                                          "B24", ColorsManager.gray500),
+                                ),
+                              ),
+                            ),
+                            //이름 입력
+                            UserNameInputTextField(
+                                nameTextController: nameTextController),
+                            // // 성별 선택
+                            GenderChoicer(
+                              isFemalePressed: widget.isFemalePressed,
+                              isManPressed: widget.isManPressed,
+                              onPressedFemale: onPressedFemaleButton,
+                              onPressedMan: onPressedManButton,
+                            ),
+                            // 생일 입력 받기
+                            UserBirthDatePicker(
+                              birthDayTextController: birthDayTextController,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 24,
+                      left: 0,
+                      right: 0,
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        height: 52,
+                        child: LongTextButton(
+                          buttonText: "다음",
+                          color: ColorsManager.brown100,
+                          onPressedFunc: isfilledAllData
+                              ? () {
+                                  AppUser newUser = currentUser.copyWith(
+                                      nickname: widget.nickName,
+                                      birthDay: birthDayTextController.text,
+                                      gender: widget.gender,
+                                      name: nameTextController.text);
 
-                  ElevatedButton(
-                    onPressed: isfilledAllData
-                        ? () {
-                            ;
-                            AppUser newUser = currentUser.copyWith(
-                                nickname: widget.nickName,
-                                birthDay: birthDayTextController.text,
-                                gender: widget.gender,
-                                name: nameTextController.text);
-                            viewModel.onEvent(
-                              AddUserInfo(newUser),
-                              callback: () {
-                                Navigator.pushNamed(context, Routes.homeRoute);
-                              },
-                            );
-                          }
-                        : null,
-                    child: Text("저장하기"),
-                  )
-                ],
+                                  viewModel.onEvent(
+                                    AddUserInfo(newUser),
+                                    callback: () {
+                                      Navigator.pushNamed(
+                                          context, Routes.homeRoute);
+                                    },
+                                  );
+                                }
+                              : null,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           );
@@ -118,11 +156,17 @@ class _RestInfoAddtionalPageState extends State<RestInfoAddtionalPage> {
   bool checkUserDataFill() {
     bool isfilledName = false;
     bool isfilledBirthDay = false;
+    bool isNameVaild = false;
 
-    if (nameTextController.text != "") isfilledName = true;
+    if (nameTextController.text != "") {
+      isfilledName = true;
+      String? erroMessage = checkAbleNameRule(nameTextController.text);
+      if (erroMessage == null) isNameVaild = true;
+    }
+
     if (birthDayTextController.text != "") isfilledBirthDay = true;
 
-    return (isfilledName && isfilledBirthDay);
+    return (isfilledName && isfilledBirthDay && isNameVaild);
   }
 
   //GenderChoicer()에서에 파라미터로 보내서 부모 state를 변동 시킨다.
