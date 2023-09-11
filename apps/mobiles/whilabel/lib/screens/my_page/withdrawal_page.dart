b97631b0b4b5d1193018e2b9363d6687af8d559f
hihibara <joungminseong@Joungui-MacBookAir.local>
2023-09-11 16:53:10 +0900

    [feature] modify import path

--- apps/mobiles/whilabel/lib/screens/my_page/withdrawal_page.dart
+++ apps/mobiles/whilabel/lib/screens/my_page/withdrawal_page.dart
@@ -4,7 +4,7 @@ import 'package:whilabel/screens/constants/path/svg_icon_paths.dart';
 import 'package:whilabel/screens/constants/text_styles_manager.dart';
 import 'package:whilabel/screens/constants/whilabel_design_setting.dart';
 import 'package:whilabel/screens/global/widgets/long_text_button.dart';
-import 'package:whilabel/screens/home/grid/widgets/app_bars.dart';
+import 'package:whilabel/screens/global/widgets/app_bars.dart';
 import 'package:whilabel/screens/my_page/widgets/functions/show_dialog.dart';
 
 // ignore: must_be_immutable
