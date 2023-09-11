74ebcabaefc3ee119320a70afa5592ee18bfb4c9
choejong-won <bmsyg987@gmail.com>
2023-09-11 11:56:58 +0900

    replace global var to local var remove json import remove global var file import

--- apps/batches/wb_bottler_collector/wb_whisky_belonging_to_bottler_collector/wb_whisky_collector_link/wb_whisky_link_belonging_to_bottler_collector_directed_acyclic_execution.py
+++ apps/batches/wb_bottler_collector/wb_whisky_belonging_to_bottler_collector/wb_whisky_collector_link/wb_whisky_link_belonging_to_bottler_collector_directed_acyclic_execution.py
@@ -1,8 +1,8 @@
-from apps.batches.wb_whisky_collector.wb_whisky_collector_link.wb_whisky_collector_link_values import \
-    whisky_collect_link_scrap as link_scrap
 from apps.batches.wb_libs import wb_libs_func
 from apps.batches.wb_whisky_collector.wb_whisky_collector_link import wb_whisky_collector_link_apply_func
 from apps.batches.wb_libs.enums import BatchType
+from apps.batches.wb_libs.constants import field_map
+
 def whisky_link_bottler_collector_executions(batch_type : BatchType):
     """
        whisky_link_bottler_collector_executions.
@@ -13,18 +13,19 @@ def whisky_link_bottler_collector_executions(batch_type : BatchType):
                scrap_main 에서 위스키 링크  스케줄링 함수
     """
 
+    scrap_dict = wb_libs_func.initialize_dict(field_map['wb_whisky_link'])
+
     current_date = wb_libs_func.get_current_date()
 
     wb_whisky_collector_link_apply_func.collect(batch_type=BatchType.BOTTER_PRE.value,
-                                                current_date=current_date)
+                                                current_date=current_date,whisky_link_scrap=scrap_dict)
 
     # save as csv file
-    result_df = wb_libs_func.convert_to_df(link_scrap)
+    result_df = wb_libs_func.convert_to_df(scrap_dict)
     result_df = wb_libs_func.remove_duplicated_link(result_df) # 위스키 링크 중복 수집 문제 임시방편 해결
     csv_path = f'results/{current_date}/csv/link/'
     wb_libs_func.save_to_csv(result_df, csv_path, batch_type.value)
 
     # save as json file
-    result_json = json.loads(link_scrap.to_json())
     json_path = f'results/{current_date}/json/link/'
-    wb_libs_func.save_to_json(result_json, json_path, batch_type.value)
+    wb_libs_func.save_to_json(scrap_dict, json_path, batch_type.value)
