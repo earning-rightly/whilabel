82c7b2ee1a0649233f95e7e1ad235631a090f155
hihibara <joungminseong@Joungui-MacBookAir.local>
2023-09-14 16:11:39 +0900

    [feature] Add CarmeraEvent()

---
+++ apps/mobiles/whilabel/lib/screens/camera/view_model/camera_event.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+import 'package:flutter/widgets.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'camera_event.freezed.dart';
+
+@freezed
+abstract class CarmeraEvent with _$CarmeraEvent {
+  const factory CarmeraEvent.useBarCodeScanner(BuildContext context) =
+      useBarCodeScanner;
+  const factory CarmeraEvent.searchWhiskeyWithBarcode(String whiskeyBarcode) =
+      searchWhiskeyWithBarcode;
+  const factory CarmeraEvent.saveImageFile(File imageFile) = saveImageFile;
+}
