118bfde7c67b590535734f3dba1f0f786a5d09cc
hihibara <joungminseong@Joungui-MacBookAir.local>
2023-09-18 18:11:48 +0900

    [feature] Add ArchivingPostRepository()
    firebase에 있는 archiving_post collection에 접근하는 기능을
     가지고 있는 클래스입니다.
     archiving_post에 접근하고자 하면 이 클래스를 사용해야 합니다

---
+++ apps/mobiles/whilabel/lib/domain/post/firebase_archiving_post_repository_impl.dart
@@ -0,0 +1,34 @@
+import 'package:whilabel/data/post/archiving_post.dart';
+import 'package:whilabel/domain/post/archiving_post_repository.dart';
+
+class FirestoreArchivingPostRepositoryImple implements ArchivingPostRepository {
+  final ArchivingPostCollectionReference _archivingPostRef;
+
+  FirestoreArchivingPostRepositoryImple(
+    this._archivingPostRef,
+  );
+  @override
+  Future<void> insertArchivingPost(ArchivingPost archivingPost) async {
+    _archivingPostRef.add(archivingPost);
+  }
+
+  @override
+  Future<List<ArchivingPost>> getArchivingPosts(String userID) async {
+    List<ArchivingPost> archivingPostList = [];
+    final querySnapshot =
+        await _archivingPostRef.whereUserId(isEqualTo: userID).get();
+
+    for (ArchivingPostQueryDocumentSnapshot eachSnaoshot
+        in querySnapshot.docs) {
+      final ArchivingPost data = eachSnaoshot.data;
+      archivingPostList.add(data);
+    }
+    return archivingPostList;
+  }
+
+  @override
+  Future<ArchivingPost?> getArchivingPost(String uid, String postId) {
+    // TODO: implement getArchivingPost
+    throw UnimplementedError();
+  }
+}
