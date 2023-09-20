bf8c4fb7782df2190c775f348bf721ac8e185bca
hihibara <joungminseong@Joungui-MacBookAir.local>
2023-09-20 14:06:09 +0900

    [feature] modify data model
     firebase에 있는 데이터 타입과 동일하게 데이터 모델을 수정

--- apps/mobiles/whilabel/lib/data/whisky/whisky_category.dart
+++ apps/mobiles/whilabel/lib/data/whisky/whisky_category.dart
@@ -1,19 +1,34 @@
 import 'package:json_annotation/json_annotation.dart';
 
 enum WhiskyCategory {
-  @JsonValue("american_whisky") americanWhisky,
-  @JsonValue("blend") blend,
-  @JsonValue("blended_grain") blendedGrain,
-  @JsonValue("blended_malt") blendedMalt,
-  @JsonValue("bourbon") bourbon,
-  @JsonValue("canadian_whisky") canadianWhisky,
-  @JsonValue("corn") corn,
-  @JsonValue("rice") rice,
-  @JsonValue("rye") rye,
-  @JsonValue("single_grain") singleGrain,
-  @JsonValue("single_malt") singleMalt,
-  @JsonValue("single_pod_still") singlePodStill,
-  @JsonValue("spirit") spirit,
-  @JsonValue("tennessee") tennessee,
-  @JsonValue("wheat") wheat,
+  @JsonValue("american_whisky")
+  americanWhisky,
+  @JsonValue("blend")
+  blend,
+  @JsonValue("blended_grain")
+  blendedGrain,
+  @JsonValue("blended_malt")
+  blendedMalt,
+  @JsonValue("bourbon")
+  bourbon,
+  @JsonValue("canadian_whisky")
+  canadianWhisky,
+  @JsonValue("corn")
+  corn,
+  @JsonValue("rice")
+  rice,
+  @JsonValue("rye")
+  rye,
+  @JsonValue("Single Grain")
+  singleGrain,
+  @JsonValue("Single Malt")
+  singleMalt,
+  @JsonValue("single_pod_still")
+  singlePodStill,
+  @JsonValue("spirit")
+  spirit,
+  @JsonValue("tennessee")
+  tennessee,
+  @JsonValue("wheat")
+  wheat,
 }
