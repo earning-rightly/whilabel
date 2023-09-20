696c319f35ee505c506d48ac6aa5435566a7e6b0
2821char <109503403+hihibara@users.noreply.github.com>
2023-09-20 15:07:03 +0900

    [Feature]WhiskyCritiqueView에 관한 페이지 & 기능 (#139)
    * Feature/add whiskey critique view (#137)
    
    * [feature] Add WhiskyCritiqueView
    
     유저가 바코드로 검색한 위스키 정보에 자신의 생각을
     비평하는 view입니다
    
     - 파일 이름을 수정
     - 피그마 디자인 적용
    
    * [feature] Add WhiskyCritiqueView' widgets
    
     1. CritiqueViewWhiskyInfoFooter :
        - WhiskyCritiqueView화면 하단에 고정되어서 유저가 검색한
          위스키 이름, 사진, 도수, 지역을 보여줌
        - 저장하기 버튼을 누르면 DB에 저장된니다.
     2. FlavorRecorder : 바디감, 향, 비트에 대한 정보를 받는 widget입니다
    
    * [feature] Add screens/global's widgets
    
     1. TextFieldLengthCounter :
       - TextField에서최대 글자수에 따라서 볂하는 현재 글자 수를 보여줍니다
    
    * Feature/add screens global (#138)
    
    * [feature] change createStarRating()
    
    - color change
    - item size fix
    - onRatingUpdate function 추가
    
    * [feature] Add createLargeTextFieldStyle()
    
    다른 TextField보다 큰 UI를가진 TextFiled에 사용됩니다
    
    ---------
    
    Co-authored-by: hihibara <joungminseong@Joungui-MacBookAir.local>
    
    ---------
    
    Co-authored-by: hihibara <joungminseong@Joungui-MacBookAir.local>
    
    * [Feature] Add  WhiskyCritiqueViewModel 추가 (#141)
    
    * [feature] Add WhiskyCritiqueEvnet
    
    * [feature] Add class WhiskyCritiqueState()
    
    * [feature] Add WhiskyCritiqueViewModel()
    
     WhiskyCritiqueView에서 사용될 viewModel입니다.
     전역 provider에서 사용되는 것이 아니라 WhiskyCritiqueViewModel안에서만
     사용될 예정입니다
    
    ---------
    
    Co-authored-by: hihibara <joungminseong@Joungui-MacBookAir.local>
    
    ---------
    
    Co-authored-by: hihibara <joungminseong@Joungui-MacBookAir.local>

---
+++ apps/mobiles/whilabel/lib/screens/whisky_critique/view_model/whisky_critique_event.dart
@@ -0,0 +1,19 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:whilabel/data/taste/taste_feature.dart';
+
+part 'whisky_critique_event.freezed.dart';
+
+@freezed
+abstract class WhiskyCritiqueEvnet with _$WhiskyCritiqueEvnet {
+  const factory WhiskyCritiqueEvnet.saveArchivingPostOnDb(
+      String userid,
+      double starValue,
+      String note,
+      TasteFeature tasteFeature) = SaveArchivingPostOnDb;
+  const factory WhiskyCritiqueEvnet.addTastNoteOnProvider(String tasteNote) =
+      AddTastNoteOnProvider;
+  const factory WhiskyCritiqueEvnet.addStarValueOnProvider(double value) =
+      AddStarValueOnProvider;
+  const factory WhiskyCritiqueEvnet.addTasteFeatureOnProvider(
+      TasteFeature tasteFeature) = AddTasteFeatureOnProvider;
+}
