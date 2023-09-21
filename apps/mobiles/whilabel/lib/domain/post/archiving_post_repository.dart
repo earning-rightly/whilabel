006d2ee8639b11b1038c2b30a8e0bc2c62d21c42
hihibara <joungminseong@Joungui-MacBookAir.local>
2023-09-21 18:13:42 +0900

    [feature] Add updateArchivingPost on ArchivingPostRepository
        DB에 저장된 ArchivingPost를 업데이트 하는 기능입니다

--- apps/mobiles/whilabel/lib/domain/post/archiving_post_repository.dart
+++ apps/mobiles/whilabel/lib/domain/post/archiving_post_repository.dart
@@ -2,6 +2,7 @@ import 'package:whilabel/data/post/archiving_post.dart';
 
 abstract class ArchivingPostRepository {
   Future<void> insertArchivingPost(ArchivingPost archivingPost);
+  Future<void> updateArchivingPost(ArchivingPost archivingPost);
   Future<List<ArchivingPost>> getArchivingPosts(String uid);
-  Future<ArchivingPost?> getArchivingPost(String uid, String id);
+  Future<ArchivingPost?> getArchivingPost(String id);
 }
