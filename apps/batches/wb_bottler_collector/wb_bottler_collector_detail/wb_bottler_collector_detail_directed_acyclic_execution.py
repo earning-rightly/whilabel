9c5f356a67f9d759b5a0a728c3e8af9d832039f9
yuhyeon-kim <yuhyeon.kim@navercorp.com>
2023-09-24 00:40:04 +0900

    refactoring brand detail execution

--- apps/batches/wb_bottler_collector/wb_bottler_collector_detail/wb_bottler_collector_detail_directed_acyclic_execution.py
+++ apps/batches/wb_bottler_collector/wb_bottler_collector_detail/wb_bottler_collector_detail_directed_acyclic_execution.py
@@ -1,44 +1,43 @@
+from apps.batches.wb.bottler import bottler_detail_collector
 from apps.batches.wb.common import wb_libs_func
-from apps.batches.wb.bottler.transform import \
-    transform_raw_to_wb_bottler
+from apps.batches.wb.bottler.transform import transform_raw_to_wb_bottler
 from apps.batches.wb.common.enums import BatchType, CollectionName, BatchExecution
 from apps.batches.libs.lib_firebase.firebase_funcs import save_to_firebase
 from apps.batches.wb.common.constants import field_map
 
 
-def bottler_detail_executions(batch_type: BatchType, batch_execution: BatchExecution):
+def bottler_execution():
     """
-        distillery_detail_executions.
+        bottler_execution.
             Args:
             Note:
-                scrap_main 에서 증류소 상세정보 스케줄링 함수
+                scrap_main 에서 보틀러 상세정보 스케줄링 함수
     """
 
     scrap_dict = wb_libs_func.initialize_dict(field_map['wb_bottler_collect_detail'])
 
     current_date = wb_libs_func.get_current_date()
 
-    wb_bottler_collector_detail_func.collect_bottler_detail(BatchType.BOTTER_PRE.value, current_date,
-                                                            scrap_dict)  # 증류소 상세정보 수집 함수 호출
+    bottler_detail_collector.collect_bottler_detail(current_date, scrap_dict)
 
     # save as csv file
     result_df = wb_libs_func.convert_to_df(scrap_dict)
     csv_path = f'results/{current_date}/csv/detail/'
-    wb_libs_func.save_to_csv(result_df, csv_path, batch_type.value)
+    wb_libs_func.save_to_csv(result_df, csv_path, BatchType.BOTTLER_DETAIL.value)
 
     # save as json file
     json_path = f'results/{current_date}/json/detail/'
-    wb_libs_func.save_to_json(scrap_dict, json_path, batch_type.value)
+    wb_libs_func.save_to_json(scrap_dict, json_path, BatchType.BOTTLER_DETAIL.value)
 
     transform_result_list, transform_result_dict = transform_raw_to_wb_bottler(BatchType.BOTTLER.value)
 
     # save as csv file
     result_df = wb_libs_func.convert_to_df(transform_result_dict)
     csv_path = f'results/{current_date}/csv/transformation/'
-    wb_libs_func.save_to_csv(result_df, csv_path, batch_type.value)
+    wb_libs_func.save_to_csv(result_df, csv_path, BatchType.BOTTLER.value)
 
     # save as json file
     json_path = f'results/{current_date}/json/transformation/'
-    wb_libs_func.save_to_json(transform_result_dict, json_path, batch_type.value)
+    wb_libs_func.save_to_json(transform_result_dict, json_path, BatchType.BOTTLER.value)
 
     save_to_firebase(CollectionName.BOTTLER, transform_result_list, 'wbId', 'wbBottler')
