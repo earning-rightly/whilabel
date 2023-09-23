0554875403ca2d6c1f1655bb14268e427cdf131b
yuhyeon-kim <yuhyeon.kim@navercorp.com>
2023-09-23 22:54:26 +0900

    merge pre execution to detail execution and inlining batch_type param

--- apps/batches/scrap_scheduler.py
+++ apps/batches/scrap_scheduler.py
@@ -4,15 +4,13 @@ from apscheduler.schedulers.background import BackgroundScheduler
 # 브랜드, 증류소, bottler 사전 정보 수집
 from apps.batches.wb_brand_collector.wb_brand_collector_pre.wb_brand_collector_pre_directed_acyclic_execution import \
     brand_pre_executions
-from apps.batches.wb_distillery_collector.wb_distillery_collector_pre.wb_distillery_collector_pre_directed_acyclic_execution import \
-    distillery_pre_executions
 from apps.batches.wb_bottler_collector.wb_bottler_collector_pre.wb_bottler_collector_pre_directed_acyclic_execution import \
     bottler_pre_executions
 
 # 브랜드, 증류소, bottler 상세정보 수집
 from apps.batches.wb_brand_collector.wb_brand_collector_detail.wb_brand_collector_detail_directed_acyclic_execution import \
     brand_detail_executions
-from apps.batches.wb.distillery.distillery_detail_execution import distillery_detail_execution
+from apps.batches.wb.distillery.distillery_execution import distillery_execution
 from apps.batches.wb_bottler_collector.wb_bottler_collector_detail.wb_bottler_collector_detail_directed_acyclic_execution import \
     bottler_detail_executions
 
@@ -46,9 +44,7 @@ scheduler.add_job(whisky_link_brand_collector_executions, 'cron', day=batch_day[
 scheduler.add_job(whisky_detail_brand_collector_executions, 'cron', day=batch_day['brand_day'], hour=23, minute=30,
                   id="whisky_detail_brand_collector_executions", timezone='Asia/Seoul',kwargs={'batch_type' : BatchType.BRAND_WHISKY_DETAIL, 'batch_id' : BatchExecution.PRETEST})
 
-scheduler.add_job(distillery_pre_executions, 'cron', day=batch_day['distillery_day'], hour=17, minute=11, id="distillery_pre_executions",
-                  timezone='Asia/Seoul', kwargs={'batch_type': BatchType.DISTILLERY_PRE})
-scheduler.add_job(distillery_detail_execution, 'cron', day=batch_day['distillery_day'], hour=17, minute=59, id="distillery_detail_execution",
+scheduler.add_job(distillery_execution, 'cron', day=batch_day['distillery_day'], hour=17, minute=59, id="distillery_detail_execution",
                   timezone='Asia/Seoul',kwargs={'batch_type' : BatchType.DISTILLERY_DETAIL, 'batch_execution' : BatchExecution.PRETEST})
 scheduler.add_job(whisky_link_distillery_collector_executions, 'cron', day=batch_day['distillery_day'], hour=20, minute=0,
                   id="whisky_link_distillery_collector_executions", timezone='Asia/Seoul',kwargs={'batch_type' : BatchType.DISTILLERY_WHISKY_LINK})
---
+++ apps/batches/wb/distillery/distillery_execution.py
@@ -0,0 +1,64 @@
+from apps.batches.wb.common import wb_libs_func
+from apps.batches.wb.distillery import distillery_collector, distillery_pre_collector
+from apps.batches.wb.distillery.transform import transform_raw_to_wb_distillery
+from apps.batches.wb.common.enums import CollectionName, BatchType
+from apps.batches.libs.lib_firebase.firebase_funcs import save_to_firebase
+from apps.batches.wb.common.constants import field_map
+from apps.batches.wb.common.constants import link_map
+
+
+def distillery_execution():
+    """
+        distillery_detail_execution.
+            Note:
+                scrap_main 에서 증류소 상세정보 스케줄링 함수
+    """
+    current_date = wb_libs_func.get_current_date()
+
+    '''
+    증류소 사전 정보 수집
+    '''
+    pre_scrap_dict = wb_libs_func.initialize_dict(field_map['wb_distillery_collect_pre'])
+
+    distillery_pre_collector.collect(pre_scrap_dict, link_map['distillery'])  # 증류소 사전정보 수집 함수 호출
+
+    # save raw pre data as csv file
+    result_df = wb_libs_func.convert_to_df(pre_scrap_dict)
+    csv_path = f'results/{current_date}/csv/pre/'
+    wb_libs_func.save_to_csv(result_df, csv_path, BatchType.DISTILLERY_PRE.value)
+
+    # save raw pre data as json file
+    json_path = f'results/{current_date}/json/pre/'
+    wb_libs_func.save_to_json(pre_scrap_dict, json_path, BatchType.DISTILLERY_PRE.value)
+
+    '''
+    증류소 상세 정보 수집
+    '''
+    scrap_dict = wb_libs_func.initialize_dict(field_map['wb_distillery_collect_detail'])
+
+    distillery_collector.collect_distillery_detail(BatchType.DISTILLERY_DETAIL.value, current_date, scrap_dict)
+
+    # save raw detail data as csv file
+    result_df = wb_libs_func.convert_to_df(scrap_dict)
+    csv_path = f'results/{current_date}/csv/detail/'
+    wb_libs_func.save_to_csv(result_df, csv_path, BatchType.DISTILLERY_DETAIL.value)
+
+    # save raw detail data as json file
+    json_path = f'results/{current_date}/json/detail/'
+    wb_libs_func.save_to_json(scrap_dict, json_path, BatchType.DISTILLERY_DETAIL.value)
+
+    '''
+    가공 후 로컬 파일에 저장 + 파이어베이스 저장
+    '''
+    transform_result_list, transform_result_dict = transform_raw_to_wb_distillery(BatchType.DISTILLERY.value)
+
+    # save detail data as csv file
+    result_df = wb_libs_func.convert_to_df(transform_result_dict)
+    csv_path = f'results/{current_date}/csv/transformation/'
+    wb_libs_func.save_to_csv(result_df, csv_path, BatchType.DISTILLERY.value)
+
+    # save detail data as json file
+    json_path = f'results/{current_date}/json/transformation/'
+    wb_libs_func.save_to_json(transform_result_dict, json_path, BatchType.DISTILLERY.value)
+
+    save_to_firebase(CollectionName.DISTILLERY, transform_result_list, 'wbId', 'wbDistillery')
