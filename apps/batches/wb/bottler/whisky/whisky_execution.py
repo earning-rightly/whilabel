8e8fb6f2b8faed2f613bfc99941284cdbe642146
choejong-won <bmsyg987@gmail.com>
2023-09-26 10:39:05 +0900

    batch_id parmeter추가 write_log()을 통해 로그 기록

--- apps/batches/wb/bottler/whisky/whisky_execution.py
+++ apps/batches/wb/bottler/whisky/whisky_execution.py
@@ -1,7 +1,8 @@
 from apps.batches.transformations.wb_whisky.wb_whisky_pre_form import replace_extracted_data_with_wb_whisky_format
 from apps.batches.wb.common import wb_libs_func
 from apps.batches.wb.common.constants import field_map
-from apps.batches.wb.common.enums import BatchType
+from apps.batches.wb.common.enums import BatchType, LogMode
+from apps.batches.wb.common.wb_libs_func import write_log
 from apps.batches.wb_whisky_collector.wb_whisky_collector_detail import wb_whisky_collector_detail_func
 from apps.batches.wb_whisky_collector.wb_whisky_collector_link import wb_whisky_collector_link_apply_func
 
@@ -15,10 +16,12 @@ def whisky_execution(batch_id: BatchType):
             보틀러 위스키 수집 및 처리 실행 함수입니다.
     """
     current_date = wb_libs_func.get_current_date()
-
+    current_datetime = wb_libs_func.get_current_datetime()
     """
     보틀러 위스키 링크 수집
     """
+
+    write_log(current_date, current_datetime, LogMode.START.value, BatchType.BOTTER_WHISKY_LINK.value, batch_id.value)
 
     scrap_dict = wb_libs_func.initialize_dict(field_map['wb_whisky_link'])
 
@@ -35,9 +38,12 @@ def whisky_execution(batch_id: BatchType):
     json_path = f'results/{current_date}/json/link/'
     wb_libs_func.save_to_json(scrap_dict, json_path, BatchType.BOTTER_WHISKY_LINK.value)
 
+    write_log(current_date, current_datetime, LogMode.END.value, BatchType.BOTTER_WHISKY_LINK.value, batch_id.value)
     """
     보틀러 위스키 상세 정보 수집
     """
+
+    write_log(current_date, current_datetime, LogMode.START.value, BatchType.BOTTER_WHISKY_DETAIL.value, batch_id.value)
 
     scrap_dict = wb_libs_func.initialize_dict(field_map['wb_whisky_collect_detail'])
 
@@ -64,3 +70,5 @@ def whisky_execution(batch_id: BatchType):
     # JSON 파일로 저장
     json_path = f'results/{current_date}/json/transformation/'
     wb_libs_func.save_to_json(transform_result_dict, json_path, BatchType.BOTTER_WHISKY_DETAIL.value)
+
+    write_log(current_date, current_datetime, LogMode.END.value, BatchType.BOTTER_WHISKY_DETAIL.value, batch_id.value)
