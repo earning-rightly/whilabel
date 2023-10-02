b61051b1e510360d1d9553f9c03c7d209bfccd6f
hihibara <joungminseong@Joungui-MacBookAir.local>
2023-10-02 17:24:52 +0900

    [refactoring] add const on TextStylesManger

--- apps/mobiles/whilabel/lib/screens/_constants/text_styles_manager.dart
+++ apps/mobiles/whilabel/lib/screens/_constants/text_styles_manager.dart
@@ -3,45 +3,45 @@ import 'package:whilabel/screens/_constants/colors_manager.dart';
 
 class TextStylesManager {
 // bold style
-  static TextStyle bold28 = TextStyle(
+  static const TextStyle bold28 = TextStyle(
       fontSize: 28, fontWeight: FontWeight.bold, color: ColorsManager.gray500);
-  static TextStyle bold24 = TextStyle(
+  static const TextStyle bold24 = TextStyle(
       fontSize: 24, fontWeight: FontWeight.bold, color: ColorsManager.gray500);
-  static TextStyle bold20 = TextStyle(
+  static const TextStyle bold20 = TextStyle(
       fontSize: 20, fontWeight: FontWeight.bold, color: ColorsManager.gray500);
-  static TextStyle bold18 = TextStyle(
+  static const TextStyle bold18 = TextStyle(
       fontSize: 18, fontWeight: FontWeight.bold, color: ColorsManager.gray500);
-  static TextStyle bold16 = TextStyle(
+  static const TextStyle bold16 = TextStyle(
       fontSize: 16, fontWeight: FontWeight.bold, color: ColorsManager.gray500);
-  static TextStyle bold14 = TextStyle(
+  static const TextStyle bold14 = TextStyle(
       fontSize: 14, fontWeight: FontWeight.bold, color: ColorsManager.gray500);
-  static TextStyle bold12 = TextStyle(
+  static const TextStyle bold12 = TextStyle(
       fontSize: 12, fontWeight: FontWeight.bold, color: ColorsManager.gray500);
 
   //regular
-  static TextStyle regular24 = TextStyle(
+  static const TextStyle regular24 = TextStyle(
       fontSize: 24, fontWeight: FontWeight.w400, color: ColorsManager.gray500);
-  static TextStyle regular20 = TextStyle(
+  static const TextStyle regular20 = TextStyle(
       fontSize: 20, fontWeight: FontWeight.w400, color: ColorsManager.gray500);
-  static TextStyle regular18 = TextStyle(
+  static const TextStyle regular18 = TextStyle(
       fontSize: 18, fontWeight: FontWeight.w400, color: ColorsManager.gray500);
-  static TextStyle regular16 = TextStyle(
+  static const TextStyle regular16 = TextStyle(
       fontSize: 16, fontWeight: FontWeight.w400, color: ColorsManager.gray500);
-  static TextStyle regular14 = TextStyle(
+  static const TextStyle regular14 = TextStyle(
       fontSize: 14, fontWeight: FontWeight.w400, color: ColorsManager.gray500);
-  static TextStyle regular12 = TextStyle(
+  static const TextStyle regular12 = TextStyle(
       fontSize: 12, fontWeight: FontWeight.w400, color: ColorsManager.gray500);
 
   //medium
-  static TextStyle medium20 = TextStyle(
+  static const TextStyle medium20 = TextStyle(
       fontSize: 20, fontWeight: FontWeight.w500, color: ColorsManager.gray500);
-  static TextStyle medium18 = TextStyle(
+  static const TextStyle medium18 = TextStyle(
       fontSize: 18, fontWeight: FontWeight.w500, color: ColorsManager.gray500);
-  static TextStyle medium16 = TextStyle(
+  static const TextStyle medium16 = TextStyle(
       fontSize: 16, fontWeight: FontWeight.w500, color: ColorsManager.gray500);
-  static TextStyle medium14 = TextStyle(
+  static const TextStyle medium14 = TextStyle(
       fontSize: 14, fontWeight: FontWeight.w500, color: ColorsManager.gray500);
-  static TextStyle medium12 = TextStyle(
+  static const TextStyle medium12 = TextStyle(
       fontSize: 12, fontWeight: FontWeight.w500, color: ColorsManager.gray500);
 
   static TextStyle? createHadColorTextStyle(String textStyle, Color color) {
