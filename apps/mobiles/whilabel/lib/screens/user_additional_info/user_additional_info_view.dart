42176fa1470aa3c5c4fd8dace96a8c5c02a4a543
yuhyeon-kim <yuhyeon.kim@navercorp.com>
2023-10-24 17:09:34 +0900

    불필요한 분리된 파일 통합

--- apps/mobiles/whilabel/lib/screens/user_additional_info/user_additional_info_view.dart
+++ apps/mobiles/whilabel/lib/screens/user_additional_info/user_additional_info_view.dart
@@ -1,13 +1,130 @@
 import 'package:flutter/material.dart';
-import 'package:whilabel/screens/user_additional_info/nick_name_attional_page.dart';
+import 'package:provider/provider.dart';
+import 'package:whilabel/screens/_constants/colors_manager.dart';
+import 'package:whilabel/screens/_constants/path/svg_icon_paths.dart';
+import 'package:whilabel/screens/_constants/text_styles_manager.dart';
+import 'package:whilabel/screens/_constants/whilabel_design_setting.dart';
+import 'package:whilabel/screens/_global/functions/text_feild_rules.dart';
+import 'package:whilabel/screens/_global/functions/text_field_styles.dart';
+import 'package:whilabel/screens/_global/widgets/app_bars.dart';
+import 'package:whilabel/screens/_global/widgets/long_text_button.dart';
+import 'package:whilabel/screens/user_additional_info/rest_info_additional_page.dart';
+import 'package:whilabel/screens/user_additional_info/view_model/user_additional_info_event.dart';
+import 'package:whilabel/screens/user_additional_info/view_model/user_additional_info_view_model.dart';
+import 'package:whilabel/screens/_global/functions/show_simple_dialog.dart';
 
-// UserInfoAdditionalView의 view model과 이름이 햇갈리지 읺기 위해서
-// 추가 정보 페이지를 my 페이지에서 유저 정보 수정에서 사용할 가능성 있음
-class UserAdditionalInfoView extends StatelessWidget {
+class UserAdditionalInfoView extends StatefulWidget {
   const UserAdditionalInfoView({super.key});
 
   @override
+  State<UserAdditionalInfoView> createState() => _UserAdditionalInfoViewState();
+}
+
+// UserInfoAdditionalView의 view model과 이름이 햇갈리지 읺기 위해서
+// 추가 정보 페이지를 my 페이지에서 유저 정보 수정에서 사용할 가능성 있음
+class _UserAdditionalInfoViewState extends State<UserAdditionalInfoView> {
+  final _formKey = GlobalKey();
+  final nickNameText = TextEditingController();
+  String userNickname = "";
+  String? erroMessage = "";
+
+  @override
   Widget build(BuildContext context) {
-    return NickNameAttionalPage();
+    final viewModel = context.watch<UserAdditionalInfoViewModel>();
+
+    return Scaffold(
+        appBar: buildScaffoldAppBar(context, SvgIconPath.close, ""),
+        body: Padding(
+          padding: WhilabelPadding.basicPadding,
+          child: SafeArea(
+            child: Stack(
+              children: [
+                Positioned(
+                  child: Form(
+                    key: _formKey,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "닉네임을 설정해주세요",
+                          style: TextStylesManager.bold24,
+                        ),
+                        const SizedBox(
+                          height: 32,
+                        ),
+                        TextFormField(
+                          style: TextStylesManager.regular16,
+                          maxLength: 20,
+                          controller: nickNameText,
+                          decoration: createBasicTextFieldStyle(
+                              "[한글, 영어, 숫자, _ ] 만 가능", true),
+                          validator: (value) {
+                            erroMessage = checkAbleNickNameRule(value!);
+
+                            return erroMessage;
+                          },
+                          onChanged: (value) {
+                            setState(() {
+                              userNickname = value;
+                            });
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 24,
+                  left: 0,
+                  right: 0,
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: 52,
+                    child: LongTextButton(
+                        buttonText: "다음",
+                        color: ColorsManager.brown100,
+                        onPressedFunc: erroMessage != null
+                            ? null
+                            : () {
+                          viewModel.onEvent(
+                            CheckNickName(userNickname),
+                            callback: () {
+                              setState(() {});
+
+                              if (viewModel.state.isAbleNickName &&
+                                  viewModel.state.forbiddenWord == "") {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        RestInfoAddtionalPage(
+                                            nickName: userNickname),
+                                  ),
+                                );
+                              } else {
+                                String dialogSubTitle = "";
+
+                                if (viewModel.state.isAbleNickName ==
+                                    false) {
+                                  dialogSubTitle = "중복된 닉네임입니다";
+                                }
+                                if (viewModel.state.forbiddenWord != "") {
+                                  dialogSubTitle = "\"${viewModel.state.forbiddenWord}\" 는 금지어입니다";
+                                }
+
+                                showSimpleDialog(context, "<닉네임 사용불가>",
+                                    dialogSubTitle);
+                              }
+                            },
+                          );
+                        }),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+    );
   }
 }
