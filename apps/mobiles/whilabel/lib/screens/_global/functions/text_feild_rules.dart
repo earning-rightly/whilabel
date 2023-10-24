5bfd2d4fcb68e11c15e49cfc3d8873e4241d2bc8
yuhyeon-kim <yuhyeon.kim@navercorp.com>
2023-10-25 01:37:10 +0900

    이름 룰 수정

--- apps/mobiles/whilabel/lib/screens/_global/functions/text_feild_rules.dart
+++ apps/mobiles/whilabel/lib/screens/_global/functions/text_feild_rules.dart
@@ -1,17 +1,9 @@
 String? checkAbleNameRule(String? value) {
-  if (value == null || value.length < 2) return '2글자 이상 입력해주세요';
+  if (value == null || value.length < 2) return '2글자 이상 입력해주세요.';
 
-  if (value.toString().length > 10) return '최대 10자까지 입력 가능';
+  if (value.toString().length > 10) return '최대 10자까지 입력 가능합니다.';
 
-  if (RegExp(r'[!@#%^&*(),.?";:{}|<>-_+=]').hasMatch(value)) return '특수문자 x';
-
-  if (RegExp('[0-9]').hasMatch(value)) return '숫자 입력 x';
-
-  if (RegExp('[a-z | A-Z]').hasMatch(value)) return '영어 입력 x';
-
-  if (RegExp(' ').hasMatch(value)) return '스페이스는 포함할 수 없습니다';
-
-  if (!RegExp(r'^[ㄱ-ㅎㅏ-ㅣ가-힣]+$').hasMatch(value)) return '한글만 입력';
+  if (!RegExp(r'^[ㄱ-ㅎㅏ-ㅣ가-힣]+$').hasMatch(value)) return '한글만 입력 가능합니다.';
 
   return null;
 }
