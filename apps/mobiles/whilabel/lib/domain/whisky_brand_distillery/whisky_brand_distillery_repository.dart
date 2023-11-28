2129e98cb14c0c85adac6343860925cfcf1f97e9
joungminseong <reputable4126@gmail.com>
2023-11-28 16:51:06 +0900

    <refactoring> wbId 파라미터 barcode로 변경

--- apps/mobiles/whilabel/lib/domain/whisky_brand_distillery/whisky_brand_distillery_repository.dart
+++ apps/mobiles/whilabel/lib/domain/whisky_brand_distillery/whisky_brand_distillery_repository.dart
@@ -3,11 +3,10 @@ import 'package:whilabel/data/distillery/distillery.dart';
 import 'package:whilabel/data/whisky/whisky.dart';
 
 abstract class WhiskyBrandDistilleryRepository {
-  Future<Whisky?> getWhiskyDataWithBarcode(String wbId);
+  Future<Whisky?> getWhiskyDataWithBarcode(String barcode);
   Future<List<WhiskyQueryDocumentSnapshot>> getWhiskyDataWithName(
       String name, List<String> findedWhiskyNames,
       {WhiskyDocumentSnapshot? startAtDoc});
-
-  Future<Brand?> getBrandData(String wbId);
-  Future<Distillery?> getDistilleryData(String wbId);
+  Future<Brand?> getBrandData(String barcode);
+  Future<Distillery?> getDistilleryData(String barcode);
 }
