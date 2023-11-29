3fbbe9e7f1f52657e95f9ed38f8754f3d3a4bd15
joungminseong <reputable4126@gmail.com>
2023-11-29 21:40:24 +0900

    <feature> 각 로그인 case마다 debugPrint 추가 & FlutterSecureStorage 데이타 삭제

--- apps/mobiles/whilabel/lib/domain/use_case/user_auth/logout_use_case.dart
+++ apps/mobiles/whilabel/lib/domain/use_case/user_auth/logout_use_case.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:whilabel/data/user/sns_type.dart';
 import 'package:whilabel/domain/global_provider/current_user_status.dart';
 import 'package:whilabel/domain/login_services/apple_oauth.dart';
@@ -19,22 +20,25 @@ class LogoutUseCase {
     switch (snsType) {
       case SnsType.KAKAO:
         await KaKaoOauth().logout();
+        debugPrint("kakao logout");
         break;
 
       case SnsType.GOOGLE:
         await GoogleOauth().logout();
+        debugPrint("google logout");
         break;
 
       case SnsType.APPLE:
         await AppleOauth().logout();
+        debugPrint("apple logout");
         break;
 
       default:
         debugPrint("instatgram 과 카카오는 로그아웃 방식이 다릅니다.");
         break;
     }
-
     await FirebaseAuth.instance.signOut();
+    await const FlutterSecureStorage().deleteAll();
     await currentUserStatus.updateUserState();
 
     return true;
