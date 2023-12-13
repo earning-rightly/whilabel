fc23332230e1a1e33da5b58041623aac147ed7c1
joungminseong <reputable4126@gmail.com>
2023-12-13 22:26:28 +0900

    <feature> LoginView에 뒤로가기 누르면 종료 기능 추가

--- apps/mobiles/whilabel/lib/screens/login/login_view.dart
+++ apps/mobiles/whilabel/lib/screens/login/login_view.dart
@@ -7,7 +7,9 @@ import 'package:whilabel/domain/global_provider/current_user_status.dart';
 import 'package:whilabel/screens/_constants/path/svg_icon_paths.dart';
 import 'package:whilabel/screens/_constants/routes_manager.dart';
 import 'package:whilabel/screens/_constants/whilabel_design_setting.dart';
+import 'package:whilabel/screens/_global/functions/show_dialogs.dart';
 import 'package:whilabel/screens/_global/functions/show_simple_dialog.dart';
+import 'package:whilabel/screens/_global/widgets/back_listener.dart';
 import 'package:whilabel/screens/_global/widgets/loding_progress_indicator.dart';
 import 'package:whilabel/screens/login/view_model/login_event.dart';
 import 'package:whilabel/screens/login/view_model/login_view_model.dart';
@@ -31,90 +33,57 @@ class _LoginViewState extends State<LoginView> {
   Widget build(BuildContext context) {
     final viewModel = context.watch<LoginViewModel>();
 
-    return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Positioned(
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.centerRight,
-                      colors: [
-                        Color(0xffA87137),
-                        Color(0xff864E33),
-                      ]),
+    return BackListener(
+      onBackButtonClick: ()=> showColoseAppDialog(context),
+      child: Scaffold(
+        body: SafeArea(
+          child: Stack(
+            children: [
+              Positioned(
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.centerRight,
+                        colors: [
+                          Color(0xffA87137),
+                          Color(0xff864E33),
+                        ]),
+                  ),
                 ),
               ),
-            ),
-            Column(
-              children: [
-                Expanded(
-                    child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SvgPicture.asset(
-                      SvgIconPath.whilabelIcon,
-                      width: 270,
-                    ),
-                    SizedBox(height: WhilabelSpacing.space16),
-                    Text("즐거운 위스키 생활의 시작, 위라벨.",
-                        style: TextStylesManager.createHadColorTextStyle(
-                            "B16", ColorsManager.white))
-                  ],
-                )),
-                Container(
-                  margin: const EdgeInsets.only(bottom: 20),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16, vertical: 32),
-                    child: Column(
-                      children: [
-                        EachLoginButton(
-                          buttonText: "카카오톡으로 로그인",
-                          svgImagePath: SvgIconPath.kakaoIcon,
-                          onPressedFunc: () {
-                            startLogin();
-
-                            viewModel.onEvent(const LoginEvent.login(SnsType.KAKAO),
-                                callback: () {
-                                  // 로그인 후 이동할 경로 확인
-                                  checkNextRoute(
-                                    context: context,
-                                    userState: viewModel.state.userState,
-                                  );
-                                });
-                          },
-                        ),
-                        SizedBox(height: WhilabelSpacing.space16),
-                        EachLoginButton(
-                          buttonText: "구글 계정으로 로그인",
-                          svgImagePath: SvgIconPath.googleIcon,
-                          onPressedFunc: () {
-                            startLogin();
-
-                            viewModel.onEvent(const LoginEvent.login(SnsType.GOOGLE),
-                                callback: () {
-                                  // 로그인 후 이동할 경로 확인
-                                  checkNextRoute(
-                                    context: context,
-                                    userState: viewModel.state.userState,
-                                  );
-                                });
-                          },
-                        ),
-                        if (isiOS)
-                          ...[SizedBox(height: WhilabelSpacing.space16),
+              Column(
+                children: [
+                  Expanded(
+                      child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        SvgIconPath.whilabelIcon,
+                        width: 270,
+                      ),
+                      SizedBox(height: WhilabelSpacing.space16),
+                      Text("즐거운 위스키 생활의 시작, 위라벨.",
+                          style: TextStylesManager.createHadColorTextStyle(
+                              "B16", ColorsManager.white))
+                    ],
+                  )),
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 20),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 32),
+                      child: Column(
+                        children: [
                           EachLoginButton(
-                            buttonText: "애플 계정으로 로그인",
-                            svgImagePath: SvgIconPath.appleIcon,
-                            onPressedFunc: () async {
+                            buttonText: "카카오톡으로 로그인",
+                            svgImagePath: SvgIconPath.kakaoIcon,
+                            onPressedFunc: () {
                               startLogin();
 
-                              viewModel.onEvent(const LoginEvent.login(SnsType.APPLE),
+                              viewModel.onEvent(const LoginEvent.login(SnsType.KAKAO),
                                   callback: () {
                                     // 로그인 후 이동할 경로 확인
                                     checkNextRoute(
@@ -123,17 +92,53 @@ class _LoginViewState extends State<LoginView> {
                                     );
                                   });
                             },
-                          )]
-                      ],
+                          ),
+                          SizedBox(height: WhilabelSpacing.space16),
+                          EachLoginButton(
+                            buttonText: "구글 계정으로 로그인",
+                            svgImagePath: SvgIconPath.googleIcon,
+                            onPressedFunc: () {
+                              startLogin();
+
+                              viewModel.onEvent(const LoginEvent.login(SnsType.GOOGLE),
+                                  callback: () {
+                                    // 로그인 후 이동할 경로 확인
+                                    checkNextRoute(
+                                      context: context,
+                                      userState: viewModel.state.userState,
+                                    );
+                                  });
+                            },
+                          ),
+                          if (isiOS)
+                            ...[SizedBox(height: WhilabelSpacing.space16),
+                            EachLoginButton(
+                              buttonText: "애플 계정으로 로그인",
+                              svgImagePath: SvgIconPath.appleIcon,
+                              onPressedFunc: () async {
+                                startLogin();
+
+                                viewModel.onEvent(const LoginEvent.login(SnsType.APPLE),
+                                    callback: () {
+                                      // 로그인 후 이동할 경로 확인
+                                      checkNextRoute(
+                                        context: context,
+                                        userState: viewModel.state.userState,
+                                      );
+                                    });
+                              },
+                            )]
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            LodingProgressIndicator(
-              offstage: !_isLoginProgress,
-            )
-          ],
+                ],
+              ),
+              LodingProgressIndicator(
+                offstage: !_isLoginProgress,
+              )
+            ],
+          ),
         ),
       ),
     );
