53ae9eda4734034e85626db6039c977602ee396f
joungminseong <reputable4126@gmail.com>
2023-12-16 22:10:18 +0900

    <feature> scan logic을 세로 길이에 따라서 자르는 로직으로 변경
    사진을 세로로 4등분 후 위와 아래를 자르고 가운데만 남기는 로직

--- apps/mobiles/whilabel/lib/screens/camera/page/whisky_barcode_recognition_page.dart
+++ apps/mobiles/whilabel/lib/screens/camera/page/whisky_barcode_recognition_page.dart
@@ -4,6 +4,7 @@ import 'dart:typed_data';
 import 'package:barcode_finder/barcode_finder.dart';
 import 'package:camera/camera.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -61,19 +62,33 @@ class _WhiskyBarcodeRecognitionPageState
   Future<void> scanFileinLocalMemory(File imageFile) async {
     img.Image? decodedImage = img.decodeImage(imageFile.readAsBytesSync());
     final tempDir = await getTemporaryDirectory();
-    debugPrint("원본이미지 주소 =>>\n   ${imageFile.path}");
+    // debugPrint("원본이미지 주소 =>>\n   ${imageFile.path}");
+    debugPrint("width =>>   ${decodedImage?.width}");
+    debugPrint("height =>>   ${decodedImage?.height}");
+
 
     if (decodedImage != null) {
       // cropRect에 해당하는 부분을 decodeImageFromList() 함수를 사용하여 decodedImage로 변환합니다.
       // 이미지의 사이즈, byte가 크면 이미지 전환과 스캔하는데 오랜 시간이 걸린다.
       img.Image thumbnail = img.copyResize(decodedImage,
-          height: decodedImage.height > 850 ? 800 : decodedImage.height,
-          width: decodedImage.width > 650 ? 600 : decodedImage.width);
-      Uint8List unit8ListPng = img.encodePng(thumbnail);
+          height: decodedImage.height > 1600 ? 1600 : decodedImage.height,
+          width: decodedImage.width > 1200 ? 1200 : decodedImage.width);
+
+      int testX = 0;
+      int testY = thumbnail.height  ~/ 4;
+      var cropSizeX =thumbnail.width.toInt();
+      var cropSizeY = thumbnail.height ~/2;
+
+      img.Image cropImage = img.copyCrop(
+        thumbnail, x: testX,
+        y: testY,
+        width: cropSizeX,
+        height: cropSizeY,);
+      Uint8List unit8ListPng = img.encodePng(cropImage);
 
       File SavedPngImage =
-          File("${tempDir.path}/${Timestamp.now().millisecondsSinceEpoch}.png")
-            ..writeAsBytes(unit8ListPng);
+      File("${tempDir.path}/${Timestamp.now().millisecondsSinceEpoch}.png")
+        ..writeAsBytes(unit8ListPng);
       debugPrint("\n\nresult ==> ${SavedPngImage.path}\n\n---");
 
       setState(() {
@@ -96,6 +111,8 @@ class _WhiskyBarcodeRecognitionPageState
     try {
       String? _barcodeString = await BarcodeFinder.scanFile(path: imagePath);
       if (_barcodeString != null) {
+        debugPrint("barcode san 성공입니다!!");
+
         return _barcodeString;
       }
     } catch (e) {
