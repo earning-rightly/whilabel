23865f1f15ec9921aa432ea6e5f999219379e828
joungminseong <reputable4126@gmail.com>
2023-12-19 02:00:06 +0900

    <feature> StringManger에 hiveBox에서 사용할 String 추가

--- apps/mobiles/whilabel/lib/screens/_constants/string_manger.dart
+++ apps/mobiles/whilabel/lib/screens/_constants/string_manger.dart
@@ -1,2 +1,4 @@
 const String dot = "\u2022\t\t"; // 점 구
 const String SCAN_ERROR = "scan error";
+const String CAMERA_RULE ="camera_rule";
+const String WATCH_AGAIN_CHECKBOX= "watch_again_checkbox";
