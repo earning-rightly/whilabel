b4d200a93eba75c19a01cf9ce4eecc32c67b4692
joungminseong <reputable4126@gmail.com>
2024-01-14 00:03:50 +0900

    <feature> mock 이미지 & 이미지 주소 추가

---
+++ apps/mobiles/whilabel/lib/mock_data/_const/mock_images_path.dart
@@ -0,0 +1,18 @@
+const imageMock1 = "assets/mock_assets/mock_barcode/mock1.jpeg";
+const imageMock2 = "assets/mock_assets/mock_barcode/mock2.jpeg";
+const imageMock3 = "assets/mock_assets/mock_barcode/mock3.jpeg";
+const imageMock4 = "assets/mock_assets/mock_barcode/mock4.jpeg";
+const imageMock5 = "assets/mock_assets/mock_barcode/mock5.jpeg";
+const imageMockLarge1 = "assets/mock_assets/mock_barcode/mock_large1.jpeg";
+const imageMockLarge2 = "assets/mock_assets/mock_barcode/mock_large2.jpeg";
+const imageMockLarge3 = "assets/mock_assets/mock_barcode/mock_large3.jpeg";
+const imageMockLarge4 = "assets/mock_assets/mock_barcode/mock_large4.jpeg";
+const imageMockLarge5 = "assets/mock_assets/mock_barcode/mock_large5.jpeg";
+const imageMockSmall1 = "assets/mock_assets/mock_barcode/mock_small1.jpeg";
+const imageMockSmall2 = "assets/mock_assets/mock_barcode/mock_small2.jpeg";
+const imageMockSmall3 = "assets/mock_assets/mock_barcode/mock_small3.jpeg";
+const imageMockSmall4 = "assets/mock_assets/mock_barcode/mock_small4.jpeg";
+const imageMockSmall5 = "assets/mock_assets/mock_barcode/mock_small5.jpeg";
+
+
+
