19ec9a0c7ed96582673bc5159e4eb734c8983be9
hihibara <reputable4126@gmail.com>
2024-03-20 21:05:51 +0900

    <feature> WithdrawUseCase 추가
    firebase token을 이용해서 firebasefunction에 request 요청으로 구현

---
+++ apps/mobiles/whilabel/lib/domain/use_case/user_auth/withdraw_use_case.dart
@@ -0,0 +1,49 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
+/* firebase에 로그인한 유저의 토큰을 생성
+**/
+class WithdrawUseCase {
+  // firebase의 설정에 있는 로직
+  final String url =
+'https://us-central1-whilabel.cloudfunctions.net/withdrawUser';
+  Future<String?> call(String uid, String nickName) async {
+    // 현재 유저의 firebase token을 받아옵니다
+    final idToken = await FirebaseAuth.instance.currentUser?.getIdToken();
+
+    if (idToken != null) {
+      final header = {"authorization": 'Bearer $idToken'};
+      print('$uid \ $nickName');
+      Map<String, dynamic> _json = {'uid': uid, 'nickName': nickName};
+
+      try {
+        final response = await http
+            .post(Uri.parse(url), headers: header, body: _json);
+        debugPrint(
+            "withdrawUseCase \n${response.statusCode} \n${response.body}");
+        return response.body.toString();
+      } catch (error) {
+        debugPrint("withdrawUseCase error ===>>>\n $error");
+        return null;
+      }
+    }
+  }
+}
+
+Future<String?> withdrawUseCase(String uid, String nickName) async {
+  const String url =
+      'https://us-central1-whilabel.cloudfunctions.net/EraseUserDataOnFirebase';
+
+  Map<String, dynamic> _json = {'uid': uid, 'docId': nickName};
+
+  try {
+    final response = await http.post(Uri.parse(url), body: _json);
+    debugPrint("withdrawUseCase \n${response.body}");
+    return response.body;
+  } catch (error) {
+    debugPrint("withdrawUseCase error ===>>>\n $error");
+    return null;
+  }
+}
